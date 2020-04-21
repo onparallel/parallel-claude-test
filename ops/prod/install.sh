c4b61b8f11b613a9f4041a3add09fe1fd78e500b
Santi Albo <santialbo@gmail.com>
2020-04-21 12:37:59 +0200

    [feat] add password on staging

--- ops/prod/install.sh
+++ ops/prod/install.sh
@@ -8,6 +8,9 @@ aws s3 cp s3://parallel-builds/${BUILD_ID}.tar.gz ${BUILD_ID}.tar.gz --profile p
 tar -zxpf ${BUILD_ID}.tar.gz
 ln -s ${BUILD_ID} main
 
+# password for staging
+sudo echo "parallel:$apr1$wY1qv83a$ErfofKvlFLeIZ4r4ijEDw/" >>/etc/nginx/.htpasswd
+
 sed -i "s/#ENV#/${ENV}/g" main/ops/prod/systemd/parallel-client.service
 sed -i "s/#COMMIT_SHA#/${COMMIT_SHA}/g" main/ops/prod/nginx.conf
 sudo cp main/ops/prod/systemd/* /lib/systemd/system
