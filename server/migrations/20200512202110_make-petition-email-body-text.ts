b49169041914e93dd9858f92ad2d842e75ac5d33
Santi Albo <santialbo@gmail.com>
2020-05-12 20:25:37 +0200

    [fix] Make petition email body type text

---
+++ server/migrations/20200512202110_make-petition-email-body-text.ts
@@ -0,0 +1,13 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.alterTable("petition", (t) => {
+    t.text("email_body").alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable("petition", (t) => {
+    t.string("email_body").alter();
+  });
+}
