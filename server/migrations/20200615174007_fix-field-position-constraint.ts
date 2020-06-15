f08d14b14702cdf4682dcae30ec982ae0749a604
Santi Albo <santialbo@gmail.com>
2020-06-15 17:54:25 +0200

    [fix] Fix unique constraint on petition_field so that it can be deferred within transactions

---
+++ server/migrations/20200615174007_fix-field-position-constraint.ts
@@ -0,0 +1,23 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.raw(/* sql */ `
+    drop index "petition_field__petition_id__position"
+  `);
+  await knex.raw(/* sql */ `
+    alter table petition_field
+      add constraint "petition_field__petition_id__position" exclude (
+        petition_id with =,
+        position with =
+      ) where (deleted_at is null) deferrable initially deferred
+  `);
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.raw(/* sql */ `
+    alter table petition_field drop constraint "petition_field__petition_id__position"
+  `);
+  await knex.raw(/* sql */ `
+    create unique index "petition_field__petition_id__position" on "petition_field" ("petition_id", "position") where "deleted_at" is null
+  `);
+}
