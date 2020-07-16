2d89b52623d52d88e6afd740cf81443905296ab6
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-07-16 13:07:59 +0200

    [fix] removed constraint from petition_access table

---
+++ server/migrations/20200715173454_remove-petition-access-constraint.ts
@@ -0,0 +1,12 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  return await knex.raw(
+    /* sql */ `alter table petition_access drop constraint petition_access__reminders_check`
+  );
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return await knex.raw(/* sql */ `ALTER TABLE petition_access ADD CONSTRAINT petition_access__reminders_check CHECK (((reminders_active AND (reminders_config IS NOT NULL)) OR ((NOT reminders_active) AND (reminders_config IS NULL))))
+`);
+}
