9e3601ffb0e4eceed6c984c9cf2b66eb1dafd3e2
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-07-22 10:18:59 +0200

    [feat] migration to remove access for deleted contacts

---
+++ server/migrations/20200722093747_disable-access-for-deleted-contacts.ts
@@ -0,0 +1,12 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.raw(/* sql */ `
+    UPDATE petition_access pa 
+    SET status = 'INACTIVE' 
+    WHERE contact_id IN (
+        SELECT id FROM contact c2 WHERE c2.deleted_at IS NOT NULL 
+    )`);
+}
+
+export async function down(knex: Knex): Promise<any> {}
