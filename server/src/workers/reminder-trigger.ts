c6bb4330f8e3a37a77d3723b2a50a631dc7cdf3d
Santi Albo <santialbo@gmail.com>
2020-08-04 18:23:54 +0200

    [ch486] fix too many reminders being created and events with wrong petition_id

--- server/src/workers/reminder-trigger.ts
+++ server/src/workers/reminder-trigger.ts
@@ -1,26 +1,29 @@
 import { createCronWorker } from "./helpers/createCronWorker";
-import { chunk } from "remeda";
-import { eachOf } from "async";
+import { groupBy } from "remeda";
+import { eachLimit } from "async";
 import { calculateNextReminder } from "../util/reminderUtils";
 
 createCronWorker("reminder-trigger", async (context) => {
   const accesses = await context.petitions.getRemindableAccesses();
-  for (const batch of chunk(accesses, 10)) {
-    await eachOf(batch, async (access) => {
+  for (const [, batch] of Object.entries(
+    groupBy(accesses, (a) => a.petition_id)
+  )) {
+    // Update next reminders
+    await eachLimit(batch, 5, async (access) => {
       await context.petitions.updatePetitionAccessNextReminder(
         access.id,
         calculateNextReminder(new Date(), access.reminders_config!)
       );
-      const reminders = await context.petitions.createReminders(
-        access.petition_id,
-        accesses.map((access) => ({
-          petition_access_id: access.id,
-          status: "PROCESSING",
-          type: "AUTOMATIC",
-          created_by: `PetitionAccess:${access.id}`,
-        }))
-      );
-      await context.aws.enqueueReminders(reminders.map((r) => r.id));
     });
+    const reminders = await context.petitions.createReminders(
+      batch[0].petition_id,
+      batch.map((access) => ({
+        petition_access_id: access.id,
+        status: "PROCESSING",
+        type: "AUTOMATIC",
+        created_by: `PetitionAccess:${access.id}`,
+      }))
+    );
+    await context.aws.enqueueReminders(reminders.map((r) => r.id));
   }
 });
