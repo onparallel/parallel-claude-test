5dcb1ca51a0e7f55a73604dce13b2bcfb6f9685c
Santi Albo <santialbo@gmail.com>
2020-08-20 13:22:30 +0200

    [fix] fix exposing the dataloader correctly

--- server/src/util/fromDataLoader.ts
+++ server/src/util/fromDataLoader.ts
@@ -16,26 +16,26 @@ export function fromDataLoader<K, V, C = K>(
 ): Loader<K, V> {
   return Object.assign(
     async function (ids: K | K[], opts: FromDataLoaderOptions = {}) {
-    const { refresh, cache } = {
-      refresh: false,
-      cache: true,
-      ...opts,
-    };
-    if (refresh || !cache) {
-      for (const id of Array.isArray(ids) ? ids : [ids]) {
-        dataloader.clear(id);
+      const { refresh, cache } = {
+        refresh: false,
+        cache: true,
+        ...opts,
+      };
+      if (refresh || !cache) {
+        for (const id of Array.isArray(ids) ? ids : [ids]) {
+          dataloader.clear(id);
+        }
       }
-    }
-    const result = Array.isArray(ids)
-      ? await dataloader.loadMany(ids)
-      : await dataloader.load(ids);
-    if (!cache) {
-      for (const id of Array.isArray(ids) ? ids : [ids]) {
-        dataloader.clear(id);
+      const result = Array.isArray(ids)
+        ? await dataloader.loadMany(ids)
+        : await dataloader.load(ids);
+      if (!cache) {
+        for (const id of Array.isArray(ids) ? ids : [ids]) {
+          dataloader.clear(id);
+        }
       }
-    }
-    return result;
+      return result;
     } as any,
-    dataloader
+    { dataloader }
   );
 }
