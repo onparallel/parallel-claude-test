6e4dfe26de6856a4e174c76355b5f48735917f31
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-10-29 09:43:05 +0100

    [feat/ch738] constraint for new table

--- server/migrations/20201026133245_new-petition-signature-table.ts
+++ server/migrations/20201026133245_new-petition-signature-table.ts
@@ -1,37 +1,41 @@
 import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("petition_signature", (t) => {
-    t.increments("id");
-    t.integer("petition_id").notNullable().references("petition.id");
-    t.string("signer_email").notNullable();
-    t.string("provider").notNullable();
-    t.string("external_id");
-    t.enum(
-      "status",
-      [
-        "REQUEST_SENT",
-        "EMAIL_DELIVERED",
-        "EMAIL_BOUNCED",
-        "EMAIL_DEFERRED",
-        "DOCUMENT_DECLINED",
-        "DOCUMENT_CANCELED",
-        "DOCUMENT_EXPIRED",
-        "DOCUMENT_SIGNED",
-        "DOCUMENT_COMPLETED",
-      ],
-      {
-        useNative: true,
-        enumName: "petition_signature_status",
-      }
-    )
-      .notNullable()
-      .defaultTo("REQUEST_SENT");
-    t.jsonb("data");
-    t.timestamp("created_at")
-      .notNullable()
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
-  });
+  await knex.schema
+    .createTable("petition_signature", (t) => {
+      t.increments("id");
+      t.integer("petition_id").notNullable().references("petition.id");
+      t.string("signer_email").notNullable();
+      t.string("provider").notNullable();
+      t.string("external_id");
+      t.enum(
+        "status",
+        [
+          "REQUEST_SENT",
+          "EMAIL_DELIVERED",
+          "EMAIL_BOUNCED",
+          "EMAIL_DEFERRED",
+          "DOCUMENT_DECLINED",
+          "DOCUMENT_CANCELED",
+          "DOCUMENT_EXPIRED",
+          "DOCUMENT_SIGNED",
+          "DOCUMENT_COMPLETED",
+        ],
+        {
+          useNative: true,
+          enumName: "petition_signature_status",
+        }
+      )
+        .notNullable()
+        .defaultTo("REQUEST_SENT");
+      t.jsonb("data");
+      t.timestamp("created_at")
+        .notNullable()
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
+    })
+    .raw(
+      `create unique index "petition_signature_provider_external_id_signer_email" on "petition_signature" ("provider", "external_id", "signer_email")`
+    );
 }
 
 export async function down(knex: Knex): Promise<void> {
