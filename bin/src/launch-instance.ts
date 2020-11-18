c8af1f77dc326568672c9621f4307d1a1ca9c6a4
Santi Albo <santialbo@gmail.com>
2020-11-18 12:55:40 +0100

    [feat] update image

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -15,7 +15,7 @@ const INSTANCE_TYPES = {
   staging: "t2.small",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0eac35f5a58799826";
+const IMAGE_ID = "ami-061494ced9842c382";
 const SECURITY_GROUP_IDS = ["sg-0486098a6131eb458"];
 const IAM_ROLE = "parallel-server";
 const VPC_ID = "vpc-5356ab39";
