044c257a0f36468e21dda3268239931a48d8ee87
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2020-12-07 14:58:24 +0100

    added environment param to Sentry

--- client/utils/sentry.ts
+++ client/utils/sentry.ts
@@ -23,6 +23,7 @@ export const init = () => {
 
   Sentry.init({
     enabled: process.env.NODE_ENV === "production",
+    environment: process.env.ENV,
     integrations,
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     release: process.env.BUILD_ID,
