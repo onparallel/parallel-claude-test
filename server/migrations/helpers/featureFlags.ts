520588e3d0bb686a7213fa9122873f2a0f59f243
Santi Albo <santialbo@gmail.com>
2020-12-21 15:27:38 +0100

    [fix] fix comment

--- server/migrations/helpers/featureFlags.ts
+++ server/migrations/helpers/featureFlags.ts
@@ -32,20 +32,19 @@ export async function removeFeatureFlag(knex: Knex, featureFlag: string) {
   }>(/* sql */ `
     select unnest(enum_range(NULL::feature_flag_name)) as feature_flag
   `);
-  const previous = rows
-    .map((r) => r.feature_flag)
-    .filter((f) => f !== featureFlag);
 
   // drop foreign key to avoid errors on next queries
   await knex.schema.alterTable("feature_flag_override", (t) => {
     t.dropForeign(["feature_flag_name"]);
   });
 
-  // recreate feature_flag_name enum without PETITION_PDF_EXPORT
+  // recreate feature_flag_name enum without this feature flag
   await knex.raw(/* sql */ `
     alter type feature_flag_name rename to feature_flag_name_old;
-    create type feature_flag_name as enum (${previous
-      .map((ff) => `'${ff}'`)
+    create type feature_flag_name as enum (${rows
+      .map((r) => r.feature_flag)
+      .filter((f) => f !== featureFlag)
+      .map((f) => `'${f}'`)
       .join(",")});
     alter table feature_flag alter column "name" type feature_flag_name using "name"::varchar::feature_flag_name;
     alter table feature_flag_override alter column "feature_flag_name" type feature_flag_name using "feature_flag_name"::varchar::feature_flag_name;
