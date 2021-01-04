5c9882d75e91b457b7bd224b48a14cee11bc8598
Santi Albo <santialbo@gmail.com>
2021-01-04 17:24:00 +0100

    [fix] make fs cookie samesite lax

--- server/src/graphql/public/mutations.ts
+++ server/src/graphql/public/mutations.ts
@@ -213,7 +213,7 @@ export const publicCheckVerificationCode = mutationField(
               {
                 path: "/",
                 httpOnly: true,
-                sameSite: "strict",
+                sameSite: "lax",
                 secure: process.env.NODE_ENV === "production",
                 maxAge: 60 * 60 * 24 * 365 * 10,
               }
