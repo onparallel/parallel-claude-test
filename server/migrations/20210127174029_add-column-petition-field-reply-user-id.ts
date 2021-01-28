4c6feb9b652c240831a951a20ca37ed55ba0cf16
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-01-28 16:19:46 -0300

    [feat/ch830] new column user_id on petition_field_reply

---
+++ server/migrations/20210127174029_add-column-petition-field-reply-user-id.ts
@@ -0,0 +1,21 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("petition_field_reply", (t) => {
+    t.integer("user_id").nullable().references("user.id").defaultTo(null);
+    t.integer("petition_access_id").nullable().alter();
+  }).raw(/* sql */ `
+      alter table "petition_field_reply" add constraint "petition_field_reply__petition_access_id__user_id" check 
+      (num_nulls(petition_access_id, user_id) = 1)
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(/* sql */ `
+    alter table petition_field_reply drop constraint "petition_field_reply__petition_access_id__user_id"
+  `);
+  await knex.schema.alterTable("petition_field_reply", (t) => {
+    t.dropColumn("user_id");
+    t.integer("petition_access_id").notNullable().alter();
+  });
+}
