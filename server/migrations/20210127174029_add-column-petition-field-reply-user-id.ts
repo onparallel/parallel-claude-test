f7b09d7411c4ddcaaf2bf0c6df4cce0e802df345
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-02-02 16:17:41 -0300

    [feat/ch830] fix migration

--- server/migrations/20210127174029_add-column-petition-field-reply-user-id.ts
+++ server/migrations/20210127174029_add-column-petition-field-reply-user-id.ts
@@ -14,6 +14,12 @@ export async function down(knex: Knex): Promise<void> {
   await knex.raw(/* sql */ `
     alter table petition_field_reply drop constraint "petition_field_reply__petition_access_id__user_id"
   `);
+
+  // for avoiding conflict when making column petition_access_id not nullable
+  await knex.raw(/* sql */ `
+    DELETE from petition_field_reply WHERE petition_access_id is null
+  `);
+
   await knex.schema.alterTable("petition_field_reply", (t) => {
     t.dropColumn("user_id");
     t.integer("petition_access_id").notNullable().alter();
