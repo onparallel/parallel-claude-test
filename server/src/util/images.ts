61be29b0f51ccbc52f5352d323b63c20ffaea83e
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-02-12 16:32:59 -0300

    [hotfix] removed extension check in url when downloading logos

--- server/src/util/images.ts
+++ server/src/util/images.ts
@@ -1,16 +1,6 @@
 import fetch from "node-fetch";
 
-const ALLOWED_EXTENSIONS = ["png", "jpeg", "jpg"];
-
 export async function downloadImageBase64(url: string) {
-  const extension = url.split(".").pop();
-  if (!extension || !ALLOWED_EXTENSIONS.includes(extension)) {
-    throw new Error(
-      `URL must end with an allowed extension. url: ${url}, allowed extensions: ${JSON.stringify(
-        ALLOWED_EXTENSIONS
-      )}`
-    );
-  }
   const response = await fetch(url);
   const buffer = await response.buffer();
   return buffer.toString("base64");
