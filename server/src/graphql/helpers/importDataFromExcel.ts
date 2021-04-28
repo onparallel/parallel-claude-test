d0d8d09949c5dd7814674e6c50e3b127e10076e8
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-04-28 10:40:54 +0200

    [fix] fix for richText cells when parsing an excel file

--- server/src/graphql/helpers/importDataFromExcel.ts
+++ server/src/graphql/helpers/importDataFromExcel.ts
@@ -1,4 +1,4 @@
-import Excel from "exceljs";
+import Excel, { CellRichTextValue } from "exceljs";
 import { Stream } from "stream";
 
 /**
@@ -12,7 +12,19 @@ export async function importFromExcel(file: Stream) {
   workbook.worksheets[0].eachRow({ includeEmpty: true }, (row) => {
     const cells: string[] = [];
     row.eachCell({ includeEmpty: true }, (cell) => {
-      cells.push(cell.toString());
+      if (typeof cell.text !== "string") {
+        if ("richText" in cell.text) {
+          cells.push(
+            (cell.text as CellRichTextValue).richText
+              .map((p) => p.text)
+              .join("")
+          );
+        } else {
+          cells.push("");
+        }
+      } else {
+        cells.push(cell.text);
+      }
     });
     rows.push(cells);
   });
