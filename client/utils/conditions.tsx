d4e0e8161d8c8a5936d412cfacf63d17fa573fa7
Konstantin Klykov Lazovskaia <kklykov@gmail.com>
2021-04-29 15:16:58 +0300

    Update client/utils/conditions.tsx
    Añadido tipo "ReactNode" al children
    
    Co-authored-by: Santi Albo <santialbo@gmail.com>

--- client/utils/conditions.tsx
+++ client/utils/conditions.tsx
@@ -1,6 +1,8 @@
+import { ReactNode } from "react";
+
 interface IfProps {
   condition: any;
-  children: any;
+  children: ReactNode;
 }
 
 export const If = ({ condition, children }: IfProps) => (
