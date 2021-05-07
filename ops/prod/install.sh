7ba3559163f2ab937763364aa2fe7f069beffe93
Santi Albo <santialbo@gmail.com>
2021-05-07 19:33:08 +0200

    [fix] fix install script to use correct nginx file name

--- ops/prod/install.sh
+++ ops/prod/install.sh
@@ -9,7 +9,7 @@ tar -zxpf ${BUILD_ID}.tar.gz
 ln -s ${BUILD_ID} main
 
 sed -i "s/#ENV#/${ENV}/g" workers.sh main/ops/prod/systemd/parallel-client.service main/ops/prod/systemd/parallel-server.service
-sudo sed -i "s/#COMMIT_SHA#/${COMMIT_SHA}/g" /etc/nginx/nginx.conf
+sudo sed -i "s/#COMMIT_SHA#/${COMMIT_SHA}/g" /etc/nginx/nginx.common.conf
 sudo cp main/ops/prod/systemd/* /lib/systemd/system
 
 sudo systemctl daemon-reload
