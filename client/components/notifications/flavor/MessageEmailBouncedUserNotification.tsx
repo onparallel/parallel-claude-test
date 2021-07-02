715df5feba8eb58557d276385a6462020bff74ae
Santi Albo <santialbo@gmail.com>
2021-07-02 11:32:13 +0200

    [fix/ch1219] fix translations

--- client/components/notifications/flavor/MessageEmailBouncedUserNotification.tsx
+++ client/components/notifications/flavor/MessageEmailBouncedUserNotification.tsx
@@ -34,7 +34,7 @@ export const MessageEmailBouncedUserNotification = Object.assign(
         >
           <FormattedMessage
             id="component.notification-email-bounced.body"
-            defaultMessage="Error sending request to recipient {name}."
+            defaultMessage="Error sending petition to recipient {name}."
             values={{
               name: (
                 <ContactLink
