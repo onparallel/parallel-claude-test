d144afb229a09b3c58cfb6c3412bbfcca933933f
Santi Albo <santialbo@gmail.com>
2021-07-22 11:33:52 +0200

    [feat] remove unnecessary helper

--- client/components/petition-activity/PetitionFieldReference.tsx
+++ client/components/petition-activity/PetitionFieldReference.tsx
@@ -3,34 +3,37 @@ import { Text } from "@chakra-ui/react";
 import { PetitionFieldReference_PetitionFieldFragment } from "@parallel/graphql/__types";
 import { Maybe } from "@parallel/utils/types";
 import { FormattedMessage } from "react-intl";
-import { ellipsis } from "@parallel/utils/ellipsis";
+import { OverflownText } from "../common/OverflownText";
 
 export function PetitionFieldReference({
   field,
 }: {
   field?: Maybe<PetitionFieldReference_PetitionFieldFragment>;
 }) {
-  return (
-    <Text as="span" display="inline" marginX="2px">
-      {field ? (
-        field.title ? (
-          <Text as="strong">{ellipsis(field.title, 50)}</Text>
-        ) : (
-          <Text as="span" textStyle="hint">
-            <FormattedMessage
-              id="generic.untitled-field"
-              defaultMessage="Untitled field"
-            />
-          </Text>
-        )
-      ) : (
-        <Text as="span" textStyle="hint">
-          <FormattedMessage
-            id="timeline.reply-created-deleted-field"
-            defaultMessage="Deleted field"
-          />
-        </Text>
-      )}
+  return field ? (
+    field.title ? (
+      <OverflownText
+        as="strong"
+        display="inline-block"
+        maxWidth="30rem"
+        verticalAlign="bottom"
+      >
+        {field.title}
+      </OverflownText>
+    ) : (
+      <Text as="span" textStyle="hint">
+        <FormattedMessage
+          id="generic.untitled-field"
+          defaultMessage="Untitled field"
+        />
+      </Text>
+    )
+  ) : (
+    <Text as="span" textStyle="hint">
+      <FormattedMessage
+        id="generic.deleted-field"
+        defaultMessage="Deleted field"
+      />
     </Text>
   );
 }
--- client/utils/ellipsis.ts
+++
@@ -1,9 +0,0 @@
-export function ellipsis(
-  text: string,
-  maxLength: number,
-  prefix = "..."
-): string {
-  return text.length > maxLength
-    ? text.slice(0, maxLength - prefix.length).concat(prefix)
-    : text;
-}
