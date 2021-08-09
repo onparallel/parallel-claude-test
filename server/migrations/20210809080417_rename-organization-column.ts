9d611a614f98e099da9224b8cea42e18a8894b98
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-08-09 14:33:10 +0200

    [feat] fix migration

--- server/migrations/20210809080417_rename-organization-column.ts
+++ server/migrations/20210809080417_rename-organization-column.ts
@@ -4,6 +4,11 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("organization", (t) => {
     t.integer("logo_public_file_id").nullable().references("public_file_upload.id");
   });
+
+  // copy the id in old column "public_file_logo_id" into new col
+  await knex.raw(/* sql */ `
+    update "organization" set logo_public_file_id = public_file_logo_id where public_file_logo_id is not null;
+  `);
 }
 
 export async function down(knex: Knex): Promise<void> {
