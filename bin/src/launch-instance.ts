43ceb3d87b6b890ec345990c59a4e014b037c4ac
Santi Albo <santialbo@gmail.com>
2021-08-11 15:11:56 +0200

    [chore] update image ami

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -15,7 +15,7 @@ const INSTANCE_TYPES = {
   staging: "t2.small",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0c1c187d853c0326c";
+const IMAGE_ID = "ami-05f3effc3db23b117";
 const SECURITY_GROUP_IDS = ["sg-0486098a6131eb458"];
 const IAM_ROLE = "parallel-server";
 const VPC_ID = "vpc-5356ab39";
