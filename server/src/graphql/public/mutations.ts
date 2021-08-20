7a99bf985da4a1652e7becdc7803b183f74d197a
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-08-20 14:40:57 +0200

    Revert "[feat/ch1600] added prefix to petitions created through a public link"
    This reverts commit aca29dbe071424460abebbaaab4cb15949f7708c.

--- server/src/graphql/public/mutations.ts
+++ server/src/graphql/public/mutations.ts
@@ -1008,7 +1008,6 @@ export const publicCreateAndSendPetitionFromPublicLink = mutationField(
         ctx.petitions.loadPublicPetitionLink(args.publicPetitionLinkId),
         ctx.petitions.getPublicPetitionLinkUsersByPublicPetitionLinkId(args.publicPetitionLinkId),
       ]);
-      const template = await ctx.petitions.loadPetition(publicPetitionLink!.template_id);
 
       return await ctx.petitions.withTransaction(async (t) => {
         const [newPetition, contact] = await Promise.all([
@@ -1019,9 +1018,6 @@ export const publicCreateAndSendPetitionFromPublicLink = mutationField(
               is_template: false,
               status: "DRAFT",
               from_public_petition_link_id: args.publicPetitionLinkId,
-              name: (template!.locale === "es" ? "[Enlace] " : "[Link] ").concat(
-                publicPetitionLink!.title
-              ),
             },
             false, // do not insert permissions for the cloned petition
             t
