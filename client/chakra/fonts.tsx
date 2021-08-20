08371b5f097a5deece45a72c6fd4a324085ddd4d
Santi Albo <santialbo@gmail.com>
2021-08-20 10:55:05 +0200

    [fix] try to fix font loading

--- client/chakra/fonts.tsx
+++ client/chakra/fonts.tsx
@@ -5,9 +5,8 @@ export const fonts = /* css */ `
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''),
-        url('/static/fonts/ibm-plex-sans-v8-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/ibm-plex-sans-v8-latin-300.woff') format('woff'); /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-300.woff') format('woff'); /* Modern Browsers */
   }
   
   /* ibm-plex-sans-regular - latin */
@@ -16,9 +15,8 @@ export const fonts = /* css */ `
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''),
-        url('/static/fonts/ibm-plex-sans-v8-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/ibm-plex-sans-v8-latin-regular.woff') format('woff'); /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-regular.woff') format('woff'); /* Modern Browsers */
   }
   
   /* ibm-plex-sans-500 - latin */
@@ -27,9 +25,8 @@ export const fonts = /* css */ `
     font-style: normal;
     font-weight: 500;
     font-display: swap;
-    src: local(''),
-        url('/static/fonts/ibm-plex-sans-v8-latin-500.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/ibm-plex-sans-v8-latin-500.woff') format('woff'); /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-500.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-500.woff') format('woff'); /* Modern Browsers */
   }
   
   /* ibm-plex-sans-600 - latin */
@@ -38,9 +35,8 @@ export const fonts = /* css */ `
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''),
-        url('/static/fonts/ibm-plex-sans-v8-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/ibm-plex-sans-v8-latin-600.woff') format('woff'); /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/ibm-plex-sans-v8-latin-600.woff') format('woff'); /* Modern Browsers */
   }
   
   /* source-sans-pro-regular - latin */
@@ -48,9 +44,8 @@ export const fonts = /* css */ `
     font-family: 'Source Sans Pro';
     font-style: normal;
     font-weight: 400;
-    src: local(''),
-        url('/static/fonts/source-sans-pro-v14-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/source-sans-pro-v14-latin-regular.woff') format('woff'), /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/source-sans-pro-v14-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/source-sans-pro-v14-latin-regular.woff') format('woff'), /* Modern Browsers */
   }
 
   /* source-sans-pro-600 - latin */
@@ -58,8 +53,7 @@ export const fonts = /* css */ `
     font-family: 'Source Sans Pro';
     font-style: normal;
     font-weight: 600;
-    src: local(''),
-        url('/static/fonts/source-sans-pro-v14-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
-        url('/static/fonts/source-sans-pro-v14-latin-600.woff') format('woff'), /* Modern Browsers */
+    src: url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/source-sans-pro-v14-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+        url('${process.env.NEXT_PUBLIC_ASSETS_URL}/static/fonts/source-sans-pro-v14-latin-600.woff') format('woff'), /* Modern Browsers */
   }
 `.replace(/\/\*.*\*\//g, "");
