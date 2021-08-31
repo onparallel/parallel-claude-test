c71dec46d753a9e70b8da4e94ca7e8ba6a019bb0
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-08-31 17:37:47 +0200

    [feat/ch1415] update "forgot password" email template

--- server/src/emails/components/ForgotPassword.tsx
+++ server/src/emails/components/ForgotPassword.tsx
@@ -2,14 +2,14 @@ import { MjmlColumn, MjmlSection, MjmlText } from "mjml-react";
 import outdent from "outdent";
 import { FormattedMessage, IntlShape } from "react-intl";
 import { Email } from "../buildEmail";
-import { Button } from "../common/Button";
 import { Closing } from "../common/Closing";
 import { Greeting } from "../common/Greeting";
 import { Layout, LayoutProps } from "../common/Layout";
+import { closing, greeting } from "../common/texts";
 
 export type ForgotPasswordProps = {
   name: string | null;
-  resetUrl: string;
+  verificationCode: string;
 } & LayoutProps;
 
 const email: Email<ForgotPasswordProps> = {
@@ -19,30 +19,55 @@ const email: Email<ForgotPasswordProps> = {
       defaultMessage: "Parallel team",
     });
   },
-  subject({}, intl) {
-    return "";
+  subject({ verificationCode }, intl) {
+    return intl.formatMessage(
+      {
+        id: "forgot-password.subject",
+        defaultMessage: "Your verification code is {verificationCode}",
+      },
+      { verificationCode }
+    );
   },
-  text({ name, parallelUrl, resetUrl }: ForgotPasswordProps, intl: IntlShape) {
-    return outdent``;
+  text({ name, verificationCode }: ForgotPasswordProps, intl: IntlShape) {
+    return outdent`
+    ${greeting({ name }, intl)}
+
+    ${intl.formatMessage({
+      id: "forgot-password.verification-code-1",
+      defaultMessage: "This is the verification code you requested to change your password:",
+    })}
+
+    ${verificationCode}
+
+    ${intl.formatMessage({
+      id: "forgot-password.ignore-text",
+      defaultMessage: "If you didn't request to change your password please ignore this email.",
+    })}
+
+    ${closing({}, intl)}
+    `;
   },
-  html({ name, assetsUrl, parallelUrl, resetUrl, logoUrl, logoAlt }: ForgotPasswordProps) {
+  html({ name, assetsUrl, parallelUrl, logoUrl, logoAlt, verificationCode }: ForgotPasswordProps) {
     return (
       <Layout assetsUrl={assetsUrl} parallelUrl={parallelUrl} logoUrl={logoUrl} logoAlt={logoAlt}>
         <MjmlSection>
           <MjmlColumn>
             <Greeting name={name} />
+
             <MjmlText>
               <FormattedMessage
-                id="forgot-password.click-to-reset"
-                defaultMessage="Click on this link to reset your password:"
+                id="forgot-password.verification-code-1"
+                defaultMessage="This is the verification code you requested to change your password:"
               />
             </MjmlText>
-            <Button href={`${parallelUrl}${resetUrl}`}>
-              <FormattedMessage
-                id="forgot-password.reset-button"
-                defaultMessage="Reset my password"
-              />
-            </Button>
+
+            <MjmlSection padding="2px">
+              <MjmlColumn width="110px" borderRadius="3px" padding="10px" backgroundColor="#f0f0f0">
+                <MjmlText fontFamily="monospace" fontSize="24px" align="center" padding="0">
+                  {verificationCode}
+                </MjmlText>
+              </MjmlColumn>
+            </MjmlSection>
             <MjmlText>
               <FormattedMessage
                 id="forgot-password.ignore-text"
@@ -62,7 +87,7 @@ export const props: ForgotPasswordProps = {
   name: "Derek",
   assetsUrl: "http://localhost",
   parallelUrl: "http://localhost",
-  resetUrl: "/en/login",
   logoUrl: "http://localhost/static/emails/logo.png",
   logoAlt: "Parallel",
+  verificationCode: "ABCD",
 };
