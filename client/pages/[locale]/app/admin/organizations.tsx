4c136f2bcbb433a19154900c0719c8779212b541
Santi Albo <santialbo@gmail.com>
2021-09-10 17:48:16 +0200

    [feat/ch1572] simplify organizations table

--- client/pages/[locale]/app/admin/organizations.tsx
+++ client/pages/[locale]/app/admin/organizations.tsx
@@ -203,17 +203,16 @@ function useOrganizationColumns() {
           id: "organizations.header.user-count",
           defaultMessage: "Users",
         }),
+        cellProps: {
+          width: "1px",
+        },
         align: "right",
-        CellContent: ({ row }) => <FormattedNumber value={row.userCount} />,
-      },
-      {
-        key: "usersLimit",
-        header: intl.formatMessage({
-          id: "organizations.header.users-limit",
-          defaultMessage: "Users limit",
-        }),
-        align: "right",
-        CellContent: ({ row }) => <FormattedNumber value={row.usageLimits.users.limit} />,
+        CellContent: ({ row }) => (
+          <>
+            <FormattedNumber value={row.userCount} />/
+            <FormattedNumber value={row.usageLimits.users.limit} />
+          </>
+        ),
       },
       {
         key: "petitionsUsage",
@@ -221,17 +220,16 @@ function useOrganizationColumns() {
           id: "organizations.header.petitions-used",
           defaultMessage: "Petitions used",
         }),
+        cellProps: {
+          width: "1px",
+        },
         align: "right",
-        CellContent: ({ row }) => <FormattedNumber value={row.usageLimits.petitions.used} />,
-      },
-      {
-        key: "petitionsLimit",
-        header: intl.formatMessage({
-          id: "organizations.header.petitions-limit",
-          defaultMessage: "Petitions limit",
-        }),
-        align: "right",
-        CellContent: ({ row }) => <FormattedNumber value={row.usageLimits.petitions.limit} />,
+        CellContent: ({ row }) => (
+          <>
+            <FormattedNumber value={row.usageLimits.petitions.used} />/
+            <FormattedNumber value={row.usageLimits.petitions.limit} />
+          </>
+        ),
       },
       {
         key: "createdAt",
