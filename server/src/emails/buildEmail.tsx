fc297daac04cc9a9b2dba9dbcbf0d9ecf43eaf99
Santi Albo <santialbo@gmail.com>
2021-09-14 13:59:43 +0200

    [feat] small simplification

--- server/src/emails/buildEmail.tsx
+++ server/src/emails/buildEmail.tsx
@@ -31,13 +31,7 @@ export async function buildEmail<T>(email: Email<T>, props: T, { locale }: Email
       validationLevel: "skip",
     }
   );
-  const intl = createIntl({
-    locale,
-    messages,
-    defaultRichTextElements: {
-      b: (chunks: any) => <strong>{chunks}</strong>,
-    },
-  });
+  const intl = createIntl(intlProps);
   const text = email.text(props, intl);
   const subject = email.subject(props, intl);
   const from = email.from(props, intl);
