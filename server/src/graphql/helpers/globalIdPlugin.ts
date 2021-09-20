611f8373261ed00019c06812d3d326d974d129f7
Santi Albo <santialbo@gmail.com>
2021-09-20 13:16:07 +0200

    [fix] fix globalIdPlugin to accept .list and .nullable on field resolver method

--- server/src/graphql/helpers/globalIdPlugin.ts
+++ server/src/graphql/helpers/globalIdPlugin.ts
@@ -1,4 +1,5 @@
 import { core, dynamicOutputMethod, plugin } from "@nexus/schema";
+import { NexusOutputFieldDef } from "@nexus/schema/dist/core";
 import { ForbiddenError } from "apollo-server-errors";
 import {
   GraphQLInputType,
@@ -9,7 +10,7 @@ import {
 } from "graphql";
 import { isDefined, mapValues, omit } from "remeda";
 import { fromGlobalId, toGlobalId } from "../../util/globalId";
-import { If } from "../../util/types";
+import { If, UnwrapArray } from "../../util/types";
 
 export type GlobalIdConfig = {
   prefixName?: string;
@@ -33,22 +34,40 @@ export type GlobalIdConfigSpread<
         ];
 
 export type GlobalIdResolverConfig<TypeName extends string, FieldName extends string> = {
-  resolve: FieldResolverWithReturnType<TypeName, FieldName, number>;
+  resolve: GlobalIdFieldResolver<TypeName, FieldName>;
 };
 
-type FieldResolverWithReturnType<TypeName extends string, FieldName extends string, ReturnType> = (
+type GlobalIdFieldResolver<TypeName extends string, FieldName extends string> = (
   root: core.RootValue<TypeName>,
   args: core.ArgsValue<TypeName, FieldName>,
   context: core.GetGen<"context">,
   info: GraphQLResolveInfo
-) => core.MaybePromise<ResultValueWithReturnType<TypeName, FieldName, ReturnType>>;
+) => core.MaybePromise<GlobalIdFieldResolverReturnType<TypeName, FieldName>>;
 
-type ResultValueWithReturnType<TypeName extends string, FieldName extends string, ReturnType> = If<
-  IsNullable<core.GetGen3<"fieldTypes", TypeName, FieldName>>,
-  ReturnType | null,
-  ReturnType
+/*
+ * Build return type for resolver based on generated fieldType
+ * e.g.:
+ * - Maybe<string> -> Maybe<number>
+ * - string -> number
+ * - Maybe<string>[] -> Maybe<number>[]
+ * - Maybe<Maybe<string>[]> -> Maybe<Maybe<number>[]>
+ */
+type GlobalIdFieldResolverReturnType<
+  TypeName extends string,
+  FieldName extends string
+> = _GlobalIdFieldResolverReturnType<core.GetGen3<"fieldTypes", TypeName, FieldName>>;
+
+type _GlobalIdFieldResolverReturnType<T> = _UnwrapIfNullList<T>;
+
+type _UnwrapIfNullList<T> = If<
+  IsNullable<T>,
+  _UnwrapIfList<Exclude<T, null>> | null,
+  _UnwrapIfList<T>
 >;
+type _UnwrapIfList<T> = If<IsArray<T>, _UnwrapIfNull<UnwrapArray<T>>[], _UnwrapIfNull<T>>;
+type _UnwrapIfNull<T> = If<IsNullable<T>, number | null, number>;
 
+type IsArray<T> = any[] extends T ? true : false;
 type IsNullable<T> = null extends T ? true : false;
 
 export function globalIdArg(
@@ -109,16 +128,27 @@ export function globalIdPlugin() {
       }),
     ],
     onCreateFieldResolver({ fieldConfig }) {
-      const config = fieldConfig.extensions?.nexus?.config;
+      const config = fieldConfig.extensions?.nexus?.config as NexusOutputFieldDef;
       return async function (root, args, ctx, info, next) {
         // decode any GID args
         const _args = mapValues(args ?? {}, (argValue, argName) => {
           const type = fieldConfig.args![argName as string].type;
-          const argConfig = config.args[argName] as core.AllNexusArgsDefs;
+          const argConfig = config.args![argName as string];
           return mapGlobalIds(type, argValue, argConfig);
         });
         const result = await next(root, _args, ctx, info);
-        return config.type === "GID" ? toGlobalId(config[PREFIX_NAME], result) : result;
+        if (config.type === "GID") {
+          const prefixName = (config as any)[PREFIX_NAME] as string;
+          if (!isDefined(result)) {
+            return result;
+          } else if (config.wrapping?.includes("List") && Array.isArray(result)) {
+            return result.map((x) => (isDefined(x) ? toGlobalId(prefixName, x) : null));
+          } else {
+            return toGlobalId(prefixName, result);
+          }
+        } else {
+          return result;
+        }
       };
     },
     onInstall(b) {
