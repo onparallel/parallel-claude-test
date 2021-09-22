83673bd8dbd8d7a3de93e0959d61a687d35216f7
Santi Albo <santialbo@gmail.com>
2021-09-22 16:32:04 +0200

    [feat] update ami image

--- bin/dist/launch-instance.js
+++ bin/dist/launch-instance.js
@@ -18,7 +18,7 @@ const INSTANCE_TYPES = {
     staging: "t2.small",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-03d91be57468cdda3";
+const IMAGE_ID = "ami-00bdae3a831b8427d";
 const SECURITY_GROUP_IDS = ["sg-0486098a6131eb458"];
 const IAM_ROLE = "parallel-server";
 const VPC_ID = "vpc-5356ab39";
