4a3c256b68972e865ca64be9acd55669ed7c32ca
Konstantin <kklykov@gmail.com>
2021-10-07 11:40:47 +0200

    [feat/ch408] Refactor PublicPricingTable

--- client/components/public/pricing/PublicPricingTable.tsx
+++ client/components/public/pricing/PublicPricingTable.tsx
@@ -2,14 +2,17 @@ import { Center, Heading, Stack, Text } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import { CircleCheckFilledIcon, CircleCrossFilledIcon } from "@parallel/chakra/icons";
 import { Card, CardProps } from "@parallel/components/common/Card";
+import { Fragment } from "react";
 import { FormattedMessage } from "react-intl";
 import { usePricingList } from "../../../utils/usePricingList";
 
 export function PublicPricingTable(props: CardProps) {
   const list = usePricingList();
 
+  const plans = ["FREE", "BASIC", "PROFESSIONAL", "ENTERPRISE"];
+
   return (
-    <Card {...props} px={8} py={6}>
+    <Card {...props} padding={8} paddingTop={6}>
       <Stack spacing={6}>
         <Heading as="h2" fontSize="2xl">
           <FormattedMessage
@@ -17,7 +20,6 @@ export function PublicPricingTable(props: CardProps) {
             defaultMessage="Compare plans"
           />
         </Heading>
-
         <Stack
           spacing={8}
           sx={{
@@ -27,208 +29,121 @@ export function PublicPricingTable(props: CardProps) {
             },
             "table td, table th": {
               border: "4px solid white",
-              borderTop: 0,
-              borderBottom: 0,
+              borderY: 0,
+              paddingX: 4,
+              paddingY: 2,
+            },
+            "table tbody tr:nth-child(2n) td": {
+              backgroundColor: "white",
+            },
+            "table tbody tr:nth-child(2n+1) td": {
+              backgroundColor: "gray.75",
+            },
+            "table tbody:first-of-type tr:nth-child(2n) td": {
+              backgroundColor: "gray.75",
+            },
+            "table tbody:first-of-type tr:nth-child(2n+1) td": {
+              backgroundColor: "white",
+            },
+            "table thead:not(:first-of-type) th": {
+              paddingTop: 8,
             },
           }}
         >
-          {list.map((entry, index) => {
-            return (
-              <Table key={index}>
-                <Thead>
-                  <Tr display="flex" alignItems="center">
-                    <Th
-                      flexGrow={5}
-                      flexBasis={0}
-                      fontWeight="600"
-                      color="purple.800"
-                      px={4}
-                      py={2}
-                      fontSize="sm"
-                    >
-                      <Text>{entry.category}</Text>
-                    </Th>
-                    {index === 0 ? (
-                      <>
-                        <Th
-                          flexGrow={2}
-                          flexBasis={0}
-                          background="gray.400"
-                          color="white"
-                          fontWeight="600"
-                          px={4}
-                          py={2}
-                          textAlign="center"
-                          fontSize="sm"
-                        >
-                          <Text isTruncated>
-                            <FormattedMessage id="page.pricing.free" defaultMessage="Free" />
-                          </Text>
-                        </Th>
-                        <Th
-                          flexGrow={2}
-                          flexBasis={0}
-                          background="gray.500"
-                          color="white"
-                          fontWeight="600"
-                          px={4}
-                          py={2}
-                          textAlign="center"
-                          fontSize="sm"
-                        >
-                          <Text isTruncated>
-                            <FormattedMessage id="page.pricing.basic" defaultMessage="Basic" />
-                          </Text>
-                        </Th>
-                        <Th
-                          flexGrow={2}
-                          flexBasis={0}
-                          background="blue.700"
-                          color="white"
-                          fontWeight="600"
-                          px={4}
-                          py={2}
-                          textAlign="center"
-                          fontSize="sm"
-                        >
-                          <Text isTruncated>
-                            <FormattedMessage
-                              id="page.pricing.professional"
-                              defaultMessage="Professional"
-                            />
-                          </Text>
-                        </Th>
-                        <Th
-                          flexGrow={2}
-                          flexBasis={0}
-                          background="purple.600"
-                          color="white"
-                          fontWeight="600"
-                          px={4}
-                          py={2}
-                          textAlign="center"
-                          fontSize="sm"
-                        >
-                          <Text isTruncated>
-                            <FormattedMessage
-                              id="page.pricing.enterprise"
-                              defaultMessage="Enterprise"
-                            />
-                          </Text>
-                        </Th>
-                      </>
-                    ) : null}
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {entry.features.map((feature, i) => {
-                    const colorGrayWhite = index === 0 ? "gray.75" : "white";
-                    const colorWhiteGray = index === 0 ? "white" : "gray.75";
-
-                    return (
-                      <Tr key={i} display="flex">
-                        <Td
-                          flexGrow={5}
-                          flexBasis={0}
-                          py={2}
-                          px={4}
-                          backgroundColor={i % 2 ? colorGrayWhite : colorWhiteGray}
-                        >
-                          <Text>{feature.label}</Text>
-                        </Td>
-                        <Td
-                          flexGrow={2}
-                          flexBasis={0}
-                          px={4}
-                          py={2}
-                          backgroundColor={i % 2 ? colorGrayWhite : colorWhiteGray}
-                        >
-                          <Center>
-                            {feature.plan === "FREE" ? (
-                              <CircleCheckFilledIcon boxSize={5} color="purple.400" />
-                            ) : feature.plan === "ON_DEMAND" ? (
+          <Table>
+            {list.map((entry, index) => {
+              return (
+                <Fragment key={index}>
+                  <Thead>
+                    <Tr>
+                      <Th fontWeight="600" color="purple.800" fontSize="sm">
+                        {entry.category}
+                      </Th>
+                      {index === 0 ? (
+                        <>
+                          <Th
+                            background="gray.400"
+                            color="white"
+                            fontWeight="600"
+                            textAlign="center"
+                            fontSize="sm"
+                          >
+                            <Text isTruncated>
+                              <FormattedMessage id="page.pricing.free" defaultMessage="Free" />
+                            </Text>
+                          </Th>
+                          <Th
+                            background="gray.500"
+                            color="white"
+                            fontWeight="600"
+                            textAlign="center"
+                            fontSize="sm"
+                          >
+                            <Text isTruncated>
+                              <FormattedMessage id="page.pricing.basic" defaultMessage="Basic" />
+                            </Text>
+                          </Th>
+                          <Th
+                            background="blue.700"
+                            color="white"
+                            fontWeight="600"
+                            textAlign="center"
+                            fontSize="sm"
+                          >
+                            <Text isTruncated>
                               <FormattedMessage
-                                id="component.pricing-table.on-demand"
-                                defaultMessage="On demand"
+                                id="page.pricing.professional"
+                                defaultMessage="Professional"
                               />
-                            ) : (
-                              <CircleCrossFilledIcon boxSize={5} color="gray.400" />
-                            )}
-                          </Center>
-                        </Td>
-                        <Td
-                          flexGrow={2}
-                          flexBasis={0}
-                          px={4}
-                          py={2}
-                          backgroundColor={i % 2 ? colorGrayWhite : colorWhiteGray}
-                        >
-                          <Center>
-                            {feature.plan === "BASIC" || feature.plan === "FREE" ? (
-                              <CircleCheckFilledIcon boxSize={5} color="purple.400" />
-                            ) : feature.plan === "ON_DEMAND" ? (
+                            </Text>
+                          </Th>
+                          <Th
+                            background="purple.600"
+                            color="white"
+                            fontWeight="600"
+                            textAlign="center"
+                            fontSize="sm"
+                          >
+                            <Text isTruncated>
                               <FormattedMessage
-                                id="component.pricing-table.on-demand"
-                                defaultMessage="On demand"
+                                id="page.pricing.enterprise"
+                                defaultMessage="Enterprise"
                               />
-                            ) : (
-                              <CircleCrossFilledIcon boxSize={5} color="gray.400" />
-                            )}
-                          </Center>
-                        </Td>
-                        <Td
-                          flexGrow={2}
-                          flexBasis={0}
-                          px={4}
-                          py={2}
-                          backgroundColor={i % 2 ? colorGrayWhite : colorWhiteGray}
-                        >
-                          <Center>
-                            {feature.plan === "BASIC" ||
-                            feature.plan === "FREE" ||
-                            feature.plan === "PROFESSIONAL" ? (
-                              <CircleCheckFilledIcon boxSize={5} color="purple.400" />
-                            ) : feature.plan === "ON_DEMAND" ? (
-                              <FormattedMessage
-                                id="component.pricing-table.on-demand"
-                                defaultMessage="On demand"
-                              />
-                            ) : (
-                              <CircleCrossFilledIcon boxSize={5} color="gray.400" />
-                            )}
-                          </Center>
-                        </Td>
-                        <Td
-                          flexGrow={2}
-                          flexBasis={0}
-                          px={4}
-                          py={2}
-                          textAlign="center"
-                          backgroundColor={i % 2 ? colorGrayWhite : colorWhiteGray}
-                        >
-                          <Center>
-                            {feature.plan === "BASIC" ||
-                            feature.plan === "FREE" ||
-                            feature.plan === "PROFESSIONAL" ||
-                            feature.plan === "ENTERPRISE" ? (
-                              <CircleCheckFilledIcon boxSize={5} color="purple.400" />
-                            ) : feature.plan === "ON_DEMAND" ? (
-                              <FormattedMessage
-                                id="component.pricing-table.on-demand"
-                                defaultMessage="On demand"
-                              />
-                            ) : (
-                              <CircleCrossFilledIcon boxSize={5} color="gray.400" />
-                            )}
-                          </Center>
-                        </Td>
-                      </Tr>
-                    );
-                  })}
-                </Tbody>
-              </Table>
-            );
-          })}
+                            </Text>
+                          </Th>
+                        </>
+                      ) : null}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {entry.features.map((feature, i) => {
+                      return (
+                        <Tr key={i}>
+                          <Td>{feature.label}</Td>
+                          {plans.map((_, j) => (
+                            <Td key={j}>
+                              <Center>
+                                {plans.slice(0, j + 1).includes(feature.plan) ? (
+                                  <CircleCheckFilledIcon boxSize={5} color="purple.400" />
+                                ) : feature.plan === "ON_DEMAND" ? (
+                                  <FormattedMessage
+                                    id="component.pricing-table.on-demand"
+                                    defaultMessage="On demand"
+                                  />
+                                ) : (
+                                  <CircleCrossFilledIcon boxSize={5} color="gray.400" />
+                                )}
+                              </Center>
+                            </Td>
+                          ))}
+                        </Tr>
+                      );
+                    })}
+                  </Tbody>
+                </Fragment>
+              );
+            })}
+          </Table>
         </Stack>
       </Stack>
     </Card>
