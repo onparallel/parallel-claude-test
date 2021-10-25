f893a805d8c423c9af4ac73d2d47efe1c6e54cda
Santi Albo <santialbo@gmail.com>
2021-10-25 15:33:26 +0200

    [fix] fix petition tabs underline

--- client/components/layout/PetitionHeaderTabs.tsx
+++ client/components/layout/PetitionHeaderTabs.tsx
@@ -15,7 +15,6 @@ export const PetitionHeaderTabs = chakraForwardRef<"ul">(function PetitionHeader
       height={{ base: "40px", md: "64px" }}
       transform={{ base: "none", md: "translate(-50%)" }}
       left={{ base: 0, md: "50%" }}
-      marginBottom={{ base: "10px", md: 0 }}
       bottom="0"
       {...props}
     >
