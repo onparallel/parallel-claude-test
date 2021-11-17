9eb1f0999eb06396d46576cac8add9a75a439911
Santi Albo <santialbo@gmail.com>
2021-11-17 17:29:41 +0100

    [feat/ch1734] tweak migration

--- server/migrations/20211103102614_add-default-signature-integration.ts
+++ server/migrations/20211103102614_add-default-signature-integration.ts
@@ -1,62 +1,52 @@
 import { Knex } from "knex";
-import pMap from "p-map";
+import { difference } from "remeda";
 import { Organization, OrgIntegration } from "../src/db/__types";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema
-    .alterTable("org_integration", (t) => {
-      t.boolean("is_default").notNullable().defaultTo(false);
-      t.string("name").nullable();
-    })
-    .raw(
-      /* sql */ `
-    update org_integration set "name" = INITCAP("provider")
-  `
-    )
-    .alterTable("org_integration", (t) => {
-      t.string("name").notNullable().alter();
-    });
+  await knex.schema.alterTable("org_integration", (t) => {
+    t.boolean("is_default").notNullable().defaultTo(false);
+    t.string("name").nullable();
+  });
+  await knex.raw(/* sql */ `
+    update org_integration
+    set
+      "name" = INITCAP("provider"),
+      is_default = true`);
+  await knex.raw(
+    /* sql */ `
+      update org_integration set "name" = 'Signaturit Sandbox'
+      where (settings->>'API_KEY')::text = ?
+    `,
+    [process.env.SIGNATURIT_SANDBOX_API_KEY!]
+  );
+  await knex.schema.alterTable("org_integration", (t) => {
+    t.string("name").notNullable().alter();
+  });
 
   const organizations = await knex
     .from<Organization>("organization")
     .whereNull("deleted_at")
     .select("*");
 
-  const currentSignatureIntegrations = await knex
+  const integrations = await knex
     .from<OrgIntegration>("org_integration")
     .where({ type: "SIGNATURE", deleted_at: null })
     .select("*");
 
   // there already are some integrations using the SIGNATURIT_SANDBOX_API_KEY, so we just need to enable those
-  const alreadySetted = currentSignatureIntegrations.filter(
+  const existing = integrations.filter(
     (i) => i.settings.API_KEY === process.env.SIGNATURIT_SANDBOX_API_KEY
   );
 
-  await pMap(
-    alreadySetted,
-    async (i) => {
-      await knex
-        .from("org_integration")
-        .where({ id: i.id })
-        .update({
-          is_enabled: true,
-          settings: { ...i.settings, ENVIRONMENT: "sandbox" },
-          name: "Signaturit Sandbox",
-          updated_by: `Migration:20211103102614`,
-          updated_at: new Date(),
-        });
-    },
-    { concurrency: 1 }
+  const orgsWithoutIntegration = difference(
+    organizations.map((o) => o.id),
+    existing.map((as) => as.org_id)
   );
 
-  const alreadySettedOrgIds = alreadySetted.map((as) => as.org_id);
-
-  const newOrganizations = organizations.filter((o) => !alreadySettedOrgIds.includes(o.id));
-
-  if (newOrganizations.length > 0) {
+  if (orgsWithoutIntegration.length > 0) {
     await knex.from<OrgIntegration>("org_integration").insert(
-      newOrganizations.map((o) => ({
-        org_id: o.id,
+      orgsWithoutIntegration.map((id) => ({
+        org_id: id,
         type: "SIGNATURE",
         provider: "SIGNATURIT",
         name: "Signaturit Sandbox",
