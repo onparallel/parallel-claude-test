fae6f8ce732887794b068bf8f0f655369b39a2b6
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-11-24 13:01:12 +0000

    [feat/ch1851] added some tests for creating comments as user

---
+++ server/src/graphql/__tests__/PetitionFieldComments.test.ts
@@ -0,0 +1,209 @@
+import { gql } from "graphql-request";
+import { Knex } from "knex";
+import { USER_COGNITO_ID } from "../../../test/mocks";
+import { KNEX } from "../../db/knex";
+import { Mocks } from "../../db/repositories/__tests__/mocks";
+import { Organization, Petition, PetitionField, User } from "../../db/__types";
+import { toGlobalId } from "../../util/globalId";
+import { initServer, TestClient } from "./server";
+
+describe("GraphQL/Petition Fields Comments", () => {
+  let testClient: TestClient;
+  let mocks: Mocks;
+
+  let user: User;
+  let organization: Organization;
+
+  let petition: Petition;
+  let headingField: PetitionField;
+  let textFieldWithCommentsDisabled: PetitionField;
+
+  beforeAll(async () => {
+    testClient = await initServer();
+    const knex = testClient.container.get<Knex>(KNEX);
+    mocks = new Mocks(knex);
+
+    [organization] = await mocks.createRandomOrganizations(1, () => ({
+      name: "Parallel",
+      status: "DEV",
+    }));
+
+    [user] = await mocks.createRandomUsers(organization.id, 1, () => ({
+      cognito_id: USER_COGNITO_ID,
+      first_name: "Harvey",
+      last_name: "Specter",
+    }));
+
+    [petition] = await mocks.createRandomPetitions(organization.id, user.id, 1, () => ({
+      status: "PENDING",
+    }));
+
+    [headingField, textFieldWithCommentsDisabled] = await mocks.createRandomPetitionFields(
+      petition.id,
+      2,
+      (i) => ({
+        type: i === 0 ? "HEADING" : "TEXT",
+        options: { hasCommentsEnabled: i === 0 },
+      })
+    );
+
+    await mocks.createFeatureFlags([{ name: "INTERNAL_COMMENTS", default_value: true }]);
+  });
+
+  afterAll(async () => {
+    await testClient.stop();
+  });
+
+  describe("createPetitionFieldComment", () => {
+    it("creates a comment on a petition field", async () => {
+      const { errors, data } = await testClient.mutate({
+        mutation: gql`
+          mutation ($petitionId: GID!, $petitionFieldId: GID!, $content: String!) {
+            createPetitionFieldComment(
+              petitionId: $petitionId
+              petitionFieldId: $petitionFieldId
+              content: $content
+            ) {
+              id
+              comments {
+                content
+                isInternal
+              }
+            }
+          }
+        `,
+        variables: {
+          petitionId: toGlobalId("Petition", petition.id),
+          petitionFieldId: toGlobalId("PetitionField", headingField.id),
+          content: "Hello this is my comment",
+        },
+      });
+
+      expect(errors).toBeUndefined();
+      expect(data?.createPetitionFieldComment).toEqual({
+        id: toGlobalId("PetitionField", headingField.id),
+        comments: [
+          {
+            content: "Hello this is my comment",
+            isInternal: false,
+          },
+        ],
+      });
+    });
+
+    it("creates an internal comment on a petition field with comments disabled", async () => {
+      const { errors, data } = await testClient.mutate({
+        mutation: gql`
+          mutation (
+            $petitionId: GID!
+            $petitionFieldId: GID!
+            $content: String!
+            $isInternal: Boolean
+          ) {
+            createPetitionFieldComment(
+              petitionId: $petitionId
+              petitionFieldId: $petitionFieldId
+              content: $content
+              isInternal: $isInternal
+            ) {
+              id
+              comments {
+                content
+                isInternal
+              }
+            }
+          }
+        `,
+        variables: {
+          petitionId: toGlobalId("Petition", petition.id),
+          petitionFieldId: toGlobalId("PetitionField", textFieldWithCommentsDisabled.id),
+          content: "Hello this is my comment",
+          isInternal: true,
+        },
+      });
+
+      expect(errors).toBeUndefined();
+      expect(data?.createPetitionFieldComment).toEqual({
+        id: toGlobalId("PetitionField", textFieldWithCommentsDisabled.id),
+        comments: [
+          {
+            content: "Hello this is my comment",
+            isInternal: true,
+          },
+        ],
+      });
+    });
+
+    it("throws error if user wants to submit an internal comment without feature flag", async () => {
+      await mocks.knex.from("feature_flag").delete();
+
+      const { errors, data } = await testClient.mutate({
+        mutation: gql`
+          mutation (
+            $petitionId: GID!
+            $petitionFieldId: GID!
+            $content: String!
+            $isInternal: Boolean
+          ) {
+            createPetitionFieldComment(
+              petitionId: $petitionId
+              petitionFieldId: $petitionFieldId
+              content: $content
+              isInternal: $isInternal
+            ) {
+              id
+              comments {
+                content
+                isInternal
+              }
+            }
+          }
+        `,
+        variables: {
+          petitionId: toGlobalId("Petition", petition.id),
+          petitionFieldId: toGlobalId("PetitionField", headingField.id),
+          content: "Hello this is my comment",
+          isInternal: true,
+        },
+      });
+
+      expect(errors).toContainGraphQLError("FORBIDDEN");
+      expect(data).toBeNull();
+    });
+
+    it("throws error if user wants to submit a comment on a field with comments disabled", async () => {
+      const { errors, data } = await testClient.mutate({
+        mutation: gql`
+          mutation (
+            $petitionId: GID!
+            $petitionFieldId: GID!
+            $content: String!
+            $isInternal: Boolean
+          ) {
+            createPetitionFieldComment(
+              petitionId: $petitionId
+              petitionFieldId: $petitionFieldId
+              content: $content
+              isInternal: $isInternal
+            ) {
+              id
+              comments {
+                content
+                isInternal
+              }
+            }
+          }
+        `,
+        variables: {
+          petitionId: toGlobalId("Petition", petition.id),
+          petitionFieldId: toGlobalId("PetitionField", textFieldWithCommentsDisabled.id),
+          content: "Hello this is my comment",
+          isInternal: false,
+        },
+      });
+
+      expect(errors).toContainGraphQLError("FORBIDDEN");
+      expect(data).toBeNull();
+    });
+  });
+});
