fb18dba643778c137b85e8ee0f8a40a4065404a0
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2021-11-25 16:24:50 +0000

    [feat] fixed migration

--- server/migrations/20211125102418_rename-api-tokens-feature-flag.ts
+++ server/migrations/20211125102418_rename-api-tokens-feature-flag.ts
@@ -2,11 +2,23 @@ import { Knex } from "knex";
 import { addFeatureFlag, removeFeatureFlag } from "./helpers/featureFlags";
 
 export async function up(knex: Knex): Promise<void> {
-  await removeFeatureFlag(knex, "API_TOKENS");
   await addFeatureFlag(knex, "DEVELOPER_ACCESS", false);
+
+  await knex
+    .from("feature_flag_override")
+    .where("feature_flag_name", "API_TOKENS")
+    .update({ feature_flag_name: "DEVELOPER_ACCESS" });
+
+  await removeFeatureFlag(knex, "API_TOKENS");
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await removeFeatureFlag(knex, "DEVELOPER_ACCESS");
   await addFeatureFlag(knex, "API_TOKENS", false);
+
+  await knex
+    .from("feature_flag_override")
+    .where("feature_flag_name", "DEVELOPER_ACCESS")
+    .update({ feature_flag_name: "API_TOKENS" });
+
+  await removeFeatureFlag(knex, "DEVELOPER_ACCESS");
 }
