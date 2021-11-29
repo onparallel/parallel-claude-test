9172b07d6e0326e3368bba9868347da8c39c0b33
Santi Albo <santialbo@gmail.com>
2021-11-29 11:56:47 +0100

    [fix] fix comments migration

--- server/migrations/20211116132707_migrate-comments-enabled-to-petition-field-options.ts
+++ server/migrations/20211116132707_migrate-comments-enabled-to-petition-field-options.ts
@@ -1,31 +1,26 @@
 import { Knex } from "knex";
-import pMap from "p-map";
-import { Petition, PetitionField } from "../src/db/__types";
 
 export async function up(knex: Knex): Promise<void> {
-  const petitions = await knex.from<Petition>("petition").select("*");
-
-  await pMap(petitions, async (petition) => {
-    const fields = await knex
-      .from<PetitionField>("petition_field")
-      .where("petition_id", petition.id)
-      .select("*");
-
-    for (const field of fields) {
-      const updatedOptions = petition.comments_enabled
-        ? { ...field.options, hasCommentsEnabled: field.type === "HEADING" ? false : true }
-        : { ...field.options, hasCommentsEnabled: false };
-
-      await knex
-        .from<PetitionField>("petition_field")
-        .where("id", field.id)
-        .update("options", updatedOptions);
-    }
-  });
+  await knex.raw(
+    /* sql */ `
+    update petition_field pf
+    set options = jsonb_set(
+      pf.options::jsonb,
+      '{hasCommentsEnabled}',
+      (case
+        when not p.comments_enabled then 'false'
+        when pf.type = 'HEADING' then 'false'
+        else 'true'
+      end)::jsonb)::json
+    from petition p
+    where p.id = pf.petition_id;
+    `,
+    []
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.raw(
-    `update petition_field set options = (options::jsonb - 'hasCommentsEnabled')::json`
-  );
+  return knex.schema.raw(/* sql */ `
+    update petition_field set options = (options::jsonb - 'hasCommentsEnabled')::json
+  `);
 }
