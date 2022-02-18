953d84189ba177bba4af9c3d2245957cd1e058ec
Santi Albo <santialbo@gmail.com>
2022-02-18 17:47:20 +0100

    [fix] fix useConstant

--- client/utils/useConstant.ts
+++ client/utils/useConstant.ts
@@ -4,6 +4,9 @@ import { useRef } from "react";
  *  Like useMemo but guaranteeing the function is called just once.
  */
 export function useConstant<T>(fn: () => T): T {
-  const ref = useRef<{ value: T }>({ value: fn() });
+  const ref = useRef<{ value: T } | null>(null);
+  if (ref.current === null) {
+    ref.current = { value: fn() };
+  }
   return ref.current.value;
 }
