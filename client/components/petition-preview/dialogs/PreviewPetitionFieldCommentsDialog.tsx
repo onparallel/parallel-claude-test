68858d162f8debca906b2f4fdcf61a3864b6f40d
Konstantin <kklykov@gmail.com>
2022-02-23 14:28:57 +0300

    [feat/ch2224] Fix overflow in comments dialog when comments are disabled

--- client/components/petition-preview/dialogs/PreviewPetitionFieldCommentsDialog.tsx
+++ client/components/petition-preview/dialogs/PreviewPetitionFieldCommentsDialog.tsx
@@ -205,6 +205,7 @@ export function PreviewPetitionFieldCommentsDialog({
             </Center>
           ) : comments.length === 0 ? (
             <Flex
+              overflow="auto"
               flexDirection="column"
               paddingX={4}
               paddingY={8}
