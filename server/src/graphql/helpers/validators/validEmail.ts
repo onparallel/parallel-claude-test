424abf2d32a5eb64fc0f24943985e8940b0b621c
Konstantin <kklykov@gmail.com>
2022-02-23 10:37:52 +0300

    [feat/ch2229] Changed EMAIL regex and check domain when validate email

--- server/src/graphql/helpers/validators/validEmail.ts
+++ server/src/graphql/helpers/validators/validEmail.ts
@@ -3,18 +3,31 @@ import { FieldValidateArgsResolver } from "../validateArgsPlugin";
 import { ArgValidationError } from "../errors";
 import { MaybeArray } from "../../../util/types";
 import { unMaybeArray } from "../../../util/arrays";
+import { resolveMx } from "dns/promises";
 
 export const EMAIL_REGEX =
-  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])$/i;
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+async function hasInvalidEmail(emails: string[]) {
+  try {
+    for (const email of emails) {
+      if (!EMAIL_REGEX.test(email)) return true;
+      await resolveMx(email.split("@")[1]);
+    }
+    return false;
+  } catch {
+    return true;
+  }
+}
 
 export function validEmail<TypeName extends string, FieldName extends string>(
   prop: (args: core.ArgsValue<TypeName, FieldName>) => MaybeArray<string> | null | undefined,
   argName: string
 ) {
-  return ((_, args, ctx, info) => {
+  return (async (_, args, ctx, info) => {
     const emails = prop(args);
     if (emails) {
-      if (unMaybeArray(emails).some((email) => !EMAIL_REGEX.test(email))) {
+      if (await hasInvalidEmail(unMaybeArray(emails))) {
         throw new ArgValidationError(info, argName, `Value is not a valid email.`);
       }
     }
