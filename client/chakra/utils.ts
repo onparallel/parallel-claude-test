3995c9268fd8652a9dcb6e9a4b14e4d601c6305f
Santi Albo <santialbo@gmail.com>
2022-04-07 09:31:23 +0200

    [chore] small type change

--- client/chakra/utils.ts
+++ client/chakra/utils.ts
@@ -1,10 +1,10 @@
 import { As, ComponentWithAs, forwardRef, HTMLChakraProps } from "@chakra-ui/react";
-import { ElementRef, ForwardRefRenderFunction, Ref } from "react";
+import { ElementRef, ForwardRefRenderFunction, RefAttributes } from "react";
 
 export type WithChakraProps<T extends As, P> = P & Omit<HTMLChakraProps<T>, keyof P | "ref">;
 
 export function chakraForwardRef<T extends As, P = {}, R = ElementRef<T>>(
   render: ForwardRefRenderFunction<R, WithChakraProps<T, P>>
-): ComponentWithAs<T, WithChakraProps<T, P> & { ref?: Ref<R> }> {
+): ComponentWithAs<T, WithChakraProps<T, P> & RefAttributes<R>> {
   return forwardRef(render as any) as any;
 }
