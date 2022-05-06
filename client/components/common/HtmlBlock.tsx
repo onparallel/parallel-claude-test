b9af071d4514dfb2cecabe4e95d57b8eac8d628a
Konstantin <kklykov@gmail.com>
2022-05-06 12:12:39 +0300

    [feat/ch2547] Memoized and change links for ChakraLink with isExternal

--- client/components/common/HtmlBlock.tsx
+++ client/components/common/HtmlBlock.tsx
@@ -1,21 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link } from "@chakra-ui/react";
 import { chakraForwardRef } from "@parallel/chakra/utils";
 import DOMPurify from "dompurify";
-import parse from "html-react-parser";
+import parse, { domToReact, Element, HTMLReactParserOptions } from "html-react-parser";
+import { useMemo } from "react";
 
 export const HtmlBlock = chakraForwardRef<"div", { dangerousInnerHtml: string }>(function HtmlBlock(
   { children, dangerousInnerHtml: html, ...props },
   ref
 ) {
+  const options: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      if (domNode instanceof Element && domNode.name === "a") {
+        return (
+          <Link href={domNode.attribs.href} isExternal>
+            {domToReact(domNode.children, options)}
+          </Link>
+        );
+      }
+    },
+  };
+  const memoizedHtml = useMemo(() => {
+    return parse(DOMPurify.sanitize(html), options);
+  }, [html]);
+
   return (
-    <Box
-      ref={ref}
-      sx={{
-        a: { color: "purple.600", _hover: { color: "purple.800" } },
-      }}
-      {...props}
-    >
-      {parse(DOMPurify.sanitize(html))}
+    <Box ref={ref} {...props}>
+      {memoizedHtml}
     </Box>
   );
 });
