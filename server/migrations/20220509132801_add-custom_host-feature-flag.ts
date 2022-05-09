514e99989edd7a081a6e389227d0f211ebcd4a4e
Konstantin <kklykov@gmail.com>
2022-05-09 15:57:50 +0200

    [feat/ch2476] Update FF for current orgs with custom host not null

--- server/migrations/20220509132801_add-custom_host-feature-flag.ts
+++ server/migrations/20220509132801_add-custom_host-feature-flag.ts
@@ -3,6 +3,16 @@ import { addFeatureFlag, removeFeatureFlag } from "./helpers/featureFlags";
 
 export async function up(knex: Knex): Promise<void> {
   await addFeatureFlag(knex, "CUSTOM_HOST", false);
+
+  const organizations = await knex
+    .from("organization")
+    .whereNotNull("custom_host")
+    .andWhere("deleted_at", null)
+    .select("*");
+
+  await knex("feature_flag_override").insert(
+    organizations.map((org) => ({ feature_flag_name: "CUSTOM_HOST", org_id: org.id, value: true }))
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
