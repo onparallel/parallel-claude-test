fa80e8da1cf2dc00afc7dea664c35180d76226d1
Konstantin <kklykov@gmail.com>
2022-05-16 13:34:17 +0200

    [feat/ch2591] Number the petitions in a group sending

--- server/src/graphql/helpers/presendPetition.ts
+++ server/src/graphql/helpers/presendPetition.ts
@@ -43,7 +43,7 @@ export async function presendPetition(
         async (currentChunk, index) => {
           return await pMap(
             currentChunk,
-            async ([petition, contactIds]) => {
+            async ([petition, contactIds], _index) => {
               try {
                 const scheduledAt =
                   index === 0
@@ -78,9 +78,14 @@ export async function presendPetition(
                   userDelegate ? userDelegate : user,
                   t
                 );
+
+                const name = _index
+                  ? `${petition.name ?? args.subject} (${_index})`
+                  : petition.name ?? args.subject;
+
                 const [updatedPetition] = await ctx.petitions.updatePetition(
                   petition.id,
-                  { name: petition.name ?? args.subject, status: "PENDING", credits_used: 1 },
+                  { name, status: "PENDING", credits_used: 1 },
                   `User:${user.id}`,
                   t
                 );
