a9f0921e96c8e7479a48480b0a6fc55efe65f14e
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2022-06-02 16:35:29 +0200

    [chore] fixed index

--- server/migrations/20220601152631_update-table-indexes.ts
+++ server/migrations/20220601152631_update-table-indexes.ts
@@ -3,13 +3,13 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(/* sql */ `
     drop index "pun__unprocessed_comment_notifications";
-    CREATE INDEX pun__unprocessed_notifications ON public.petition_user_notification ("type") WHERE ((is_read = false) AND (processed_at IS NULL));
+    CREATE INDEX pun__unprocessed_notifications ON public.petition_user_notification USING btree (id) WHERE ((type = 'COMMENT_CREATED' or type = 'SIGNATURE_CANCELLED') AND is_read = false AND processed_at IS NULL);
 `);
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw(/* sql */ `
     drop index "pun__unprocessed_notifications";
-    CREATE INDEX pun__unprocessed_comment_notifications ON public.petition_user_notification USING btree (id) WHERE ((type = 'COMMENT_CREATED'::petition_user_notification_type) AND (is_read = false) AND (processed_at IS NULL));
+    CREATE INDEX pun__unprocessed_comment_notifications ON public.petition_user_notification USING btree (id) WHERE type = 'COMMENT_CREATED' AND is_read = false AND processed_at IS NULL;
 `);
 }
