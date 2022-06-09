cb572c97080d275dc1b8a93926f27e1182259e2d
Konstantin <kklykov@gmail.com>
2022-06-09 11:34:54 +0200

    [feat/ch2603] Fix MockAuth

--- server/test/mocks.ts
+++ server/test/mocks.ts
@@ -4,6 +4,7 @@ import { Response } from "node-fetch";
 import { UserAuthenticationRepository } from "../src/db/repositories/UserAuthenticationRepository";
 import { UserRepository } from "../src/db/repositories/UserRepository";
 import { User } from "../src/db/__types";
+import { RESULT } from "../src/graphql";
 import { EMAIL_REGEX } from "../src/graphql/helpers/validators/validEmail";
 import { IAnalyticsService } from "../src/services/analytics";
 import { IAuth } from "../src/services/auth";
@@ -45,6 +46,9 @@ export class MockAuth implements IAuth {
   async restoreSessionLogin() {}
   generateTempAuthToken(userId: number) {
     return `userId:${userId}`;
+  }
+  async resetTempPassword() {
+    return RESULT.SUCCESS;
   }
 }
 
