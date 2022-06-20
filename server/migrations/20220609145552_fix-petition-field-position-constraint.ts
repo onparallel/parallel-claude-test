93903ce9449a2d2048050aa2a70730de38d48c70
Santi Albo <santialbo@gmail.com>
2022-06-20 16:00:54 +0200

    [fix] fix migration and local postgres version

--- server/migrations/20220609145552_fix-petition-field-position-constraint.ts
+++ server/migrations/20220609145552_fix-petition-field-position-constraint.ts
@@ -2,7 +2,8 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(/* sql */ `
-    alter table petition_field drop constraint petition_field__petition_id__position;
+    alter table petition_field drop constraint if exists petition_field__petition_id__position;
+    drop index if exists petition_field__petition_id__position;
     alter table "petition_field" alter column "position" drop not null;
     update "petition_field" set "position" = null where "deleted_at" is not null;
     alter table "petition_field" add constraint "petition_field__petition_id__position__null_deleted_at" check (("position" is null and "deleted_at" is not null) or ("position" is not null and "deleted_at" is null));
