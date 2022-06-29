d51934dc575416aaf350fe93398b4900406489c0
Santi Albo <santialbo@gmail.com>
2022-06-29 16:09:51 +0200

    [fix] fix small mistake on purple->primary transition

--- client/components/common/TagColorSelect.tsx
+++ client/components/common/TagColorSelect.tsx
@@ -51,7 +51,7 @@ function useTagColors() {
       {
         label: intl.formatMessage({
           id: "generic.color.purple",
-          defaultMessage: "primary",
+          defaultMessage: "purple",
         }),
         value: "#DDDCF8",
       },
