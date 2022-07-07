0eb12ea8cbcfc189453a4c32ed10f6c3b20be0bd
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2022-07-07 11:30:13 -0300

    [feat] added ngrok as alternative tunnel server when localtunnel is offline

--- server/bin/launch-localtunnel.ts
+++ server/bin/launch-localtunnel.ts
@@ -1,16 +1,27 @@
-import localtunnel from "localtunnel";
 import { writeFileSync } from "fs";
+import localtunnel from "localtunnel";
+import { connect } from "ngrok";
 import { resolve } from "path";
 
 (async () => {
-  const tunnel = await localtunnel({
-    port: 4000,
-    subdomain: "parallel-localtunnel-dev", // try to use this custom subdomain, so restarts of the tunnel may keep the connection alive with 3rd party APIs
-  });
-  console.log("Your tunnel URL is:", tunnel.url);
-  writeFileSync(resolve(__dirname, "localtunnel-dev.url"), tunnel.url, "utf8");
+  let url = "";
+  try {
+    url = await withTimeout(
+      localtunnel({
+        port: 4000,
+        subdomain: "parallel-localtunnel-dev", // try to use this custom subdomain, so restarts of the tunnel may keep the connection alive with 3rd party APIs
+      }).then((t) => t.url),
+      10000
+    );
+  } catch {
+    console.log("localtunnel offline, trying with ngrok...");
+    url = await connect({ proto: "http", addr: 4000 });
+  }
 
-  tunnel.on("close", () => {
-    console.log("tunnel closed");
-  });
+  console.log("Your tunnel URL is:", url);
+  writeFileSync(resolve(__dirname, "localtunnel-dev.url"), url, "utf8");
 })();
+
+async function withTimeout<T>(fn: Promise<T>, timeout: number) {
+  return await Promise.race([new Promise<T>((_, rejected) => setTimeout(rejected, timeout)), fn]);
+}
