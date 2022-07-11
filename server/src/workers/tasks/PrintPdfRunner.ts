ee6514c6b7c2f7b721b3e00715a2514cddea8011
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2022-07-11 10:44:18 -0300

    [bug/ch2825] dont use petition name as title when exporting pdf

--- server/src/workers/tasks/PrintPdfRunner.ts
+++ server/src/workers/tasks/PrintPdfRunner.ts
@@ -41,14 +41,15 @@ export class PrintPdfRunner extends TaskRunner<"PRINT_PDF"> {
       }
 
       let documentTitle: Maybe<string> = null;
-      // if the task was started by a recipient, the title of the PDF should be the message subject instead of the petition name
       if (isDefined(this.task.petition_access_id)) {
+        // if the task was started by a recipient, the title of the PDF should be the message subject
         const [firstMessage] = await this.ctx.petitions.loadMessagesByPetitionAccessId(
           this.task.petition_access_id
         );
         documentTitle = firstMessage?.email_subject ?? null;
       } else {
-        documentTitle = petition.name;
+        // if started by an user, use the title of the signing document or no title at all
+        documentTitle = petition.signature_config?.title ?? null;
       }
 
       await this.onProgress(25);
