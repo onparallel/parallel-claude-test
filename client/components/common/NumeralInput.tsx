7a1a9bb71dc200509ba1786372d617348dc0e8b6
Santi Albo <santialbo@gmail.com>
2022-07-13 18:23:19 +0200

    [feat/ch2746] undo NumeralInput changes

--- client/components/common/NumeralInput.tsx
+++ client/components/common/NumeralInput.tsx
@@ -1,6 +1,6 @@
 import { FormControlOptions, Input, ThemingProps } from "@chakra-ui/react";
 import { chakraForwardRef } from "@parallel/chakra/utils";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useIntl } from "react-intl";
 import NumberFormat, { NumberFormatValues, SourceInfo } from "react-number-format";
 import { isDefined } from "remeda";
@@ -19,12 +19,14 @@ export const NumeralInput = chakraForwardRef<"input", NumeralInputProps>(functio
   ref
 ) {
   const intl = useIntl();
-  const formattedValue = isDefined(value)
-    ? intl.formatNumber(value, {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: decimals ?? 5,
-      })
-    : "";
+  const [_value, setValue] = useState(
+    isDefined(value)
+      ? intl.formatNumber(value, {
+          minimumFractionDigits: 0,
+          maximumFractionDigits: decimals ?? 5,
+        })
+      : ""
+  );
 
   const { decimalSeparator, thousandSeparator } = useMemo<{
     decimalSeparator: string;
@@ -38,11 +40,14 @@ export const NumeralInput = chakraForwardRef<"input", NumeralInputProps>(functio
   }, [intl.locale]);
 
   const handleOnValueChange = (values: NumberFormatValues, sourceInfo: SourceInfo) => {
+    const { formattedValue, floatValue } = values;
+    setValue(formattedValue);
+
     // Event is a Synthetic Event wrapper which holds target and other information.
     // Source tells whether the reason for this function being triggered was an 'event' or due to a 'prop' change
     const { event, source } = sourceInfo;
     if (source === "event" && (event.type === "change" || event.type === "keydown")) {
-      onChange(values.floatValue);
+      onChange(floatValue);
     }
   };
 
@@ -56,7 +61,7 @@ export const NumeralInput = chakraForwardRef<"input", NumeralInputProps>(functio
       decimalSeparator={decimalSeparator}
       decimalScale={decimals ?? 5}
       allowNegative={allowNegative ?? true}
-      value={formattedValue}
+      value={_value}
       onValueChange={handleOnValueChange}
       {...props}
     />
