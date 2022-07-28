3356bd51add29e4678e422a1c6abd57ac667daa8
Santi Albo <santialbo@gmail.com>
2022-07-28 13:13:06 +0200

    [chore] add ssh key

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -15,7 +15,7 @@ const INSTANCE_TYPES = {
   staging: "t2.medium",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0760798d793d8aa5b";
+const IMAGE_ID = "ami-0c6c89e76880aab92";
 const SECURITY_GROUP_IDS = ["sg-0486098a6131eb458"];
 const IAM_ROLE = "parallel-server";
 const SUBNET_ID = "subnet-d3cc68b9";
