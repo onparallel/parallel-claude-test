ecf746343b6c8acf8465d496f49c4f08bbedde5e
Konstantin <kklykov@gmail.com>
2022-07-28 14:40:34 +0200

    [feat/ch2846] Migrate brand_theme to organization_theme in migration

--- server/migrations/20220728074500_migrate-brand_theme-to-organization_theme.ts
+++ server/migrations/20220728074500_migrate-brand_theme-to-organization_theme.ts
@@ -3,10 +3,38 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.raw(/* sql */ `
     alter type organization_theme_type add value 'BRAND';
+    commit;
   `);
+
+  await knex.raw(
+    /* sql */ `
+    insert into "organization_theme" (org_id, name, type, is_default, data)
+    select
+      id,
+      'Default',
+      'BRAND',
+      true,
+      coalesce(brand_theme, ?::jsonb)
+    from "organization" order by id
+  `,
+    [JSON.stringify({})]
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.raw(/* sql */ `
+        with subquery as 
+        (select org_id, data from organization_theme where type = 'BRAND' and deleted_at is null)
+        update organization
+        set brand_theme = subquery.data
+        from subquery
+        where id = subquery.org_id
+    `);
+
+  await knex.raw(/* sql */ `
+    delete from organization_theme where type = 'BRAND';
+    `);
+
   await knex.raw(/* sql */ `
       alter type organization_theme_type rename to organization_theme_type_old;
       create type organization_theme_type as enum ('PDF_DOCUMENT');
