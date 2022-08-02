c00789efdd1d11ce3b411bd99704a516e8bfd9a4
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2022-08-02 10:58:14 -0300

    [feat/ch2894] added migration to clean event_logs history

---
+++ server/migrations/20220802134654_clean-petition_signature_request-event_logs.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(/* sql */ `
+    update petition_signature_request psr
+    set event_logs = coalesce((
+        select jsonb_agg(logs || jsonb_build_object('document', (logs->'document')::jsonb - 'events'))
+        from (
+    	    select jsonb_array_elements(event_logs) as logs from petition_signature_request as psr_updated where psr.id = psr_updated.id
+	    ) 
+        as signature_logs
+    ), '[]'::jsonb);
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {}
