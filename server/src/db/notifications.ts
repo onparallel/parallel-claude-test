0220933f82457701b96378bc03ab0f953170bccd
Santi Albo <santialbo@gmail.com>
2022-09-16 22:17:19 +0200

    [fix] fix type for notification events payload

--- server/src/db/notifications.ts
+++ server/src/db/notifications.ts
@@ -10,7 +10,7 @@ export type PetitionUserNotificationPayload<TType extends PetitionUserNotificati
     petition_field_id: number;
     petition_field_comment_id: number;
     /** if true, notified user is mentioned on the comment */
-    is_mentioned: boolean;
+    is_mentioned?: boolean;
   };
   MESSAGE_EMAIL_BOUNCED: {
     petition_access_id: number;
