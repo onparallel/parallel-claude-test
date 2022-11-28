95eec54de3a754d16581adfc5fce2dda4c486075
Santi Albo <santialbo@gmail.com>
2022-11-28 12:18:45 +0100

    [chore] remove PETITION_PDF_EXPORT feature flag

--- server/src/db/__types.ts
+++ server/src/db/__types.ts
@@ -11,7 +11,6 @@ type PartialProps<T, K extends keyof T = never> = Omit<T, K> & Partial<Pick<T, K
 
 export type FeatureFlagName =
   | "PETITION_SIGNATURE"
-  | "PETITION_PDF_EXPORT"
   | "HIDE_RECIPIENT_VIEW_CONTENTS"
   | "SKIP_FORWARD_SECURITY"
   | "EXPORT_CUATRECASAS"
@@ -31,7 +30,6 @@ export type FeatureFlagName =
 
 export const FeatureFlagNameValues = [
   "PETITION_SIGNATURE",
-  "PETITION_PDF_EXPORT",
   "HIDE_RECIPIENT_VIEW_CONTENTS",
   "SKIP_FORWARD_SECURITY",
   "EXPORT_CUATRECASAS",
