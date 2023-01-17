5abc8506e24f66f522eb1db1ab9d4d2bd1874d14
Santi Albo <santialbo@gmail.com>
2023-01-17 14:46:34 +0100

    [fix] fix migration

--- server/migrations/20221227093320_add-limit-to-reminders.ts
+++ server/migrations/20221227093320_add-limit-to-reminders.ts
@@ -2,12 +2,12 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.raw(/* sql */ `
-    update petition set reminders_config = reminders_config || jsonb_build_object('limit', '10'::jsonb);
+    update petition set reminders_config = reminders_config || jsonb_build_object('limit', '10'::jsonb) where reminders_config is not null;
   `);
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.raw(/* sql */ `
-    update petition set reminders_config = reminders_config::jsonb - 'limit';
+    update petition set reminders_config = reminders_config::jsonb - 'limit' where reminders_config is not null;
   `);
 }
