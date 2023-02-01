b3b99832cf308a07982692458ea3b0370c84ff9b
Santi Albo <santialbo@gmail.com>
2023-02-01 11:27:52 +0100

    [fix] fix client build issues after storybook upgrade

--- client/stories/decorators.tsx
+++ client/stories/decorators.tsx
@@ -3,15 +3,11 @@ import { Fonts } from "@parallel/chakra/fonts";
 import { theme } from "@parallel/chakra/theme";
 import { LiquidProvider } from "@parallel/utils/useLiquid";
 import { DecoratorFunction } from "@storybook/csf";
-import { ReactFramework } from "@storybook/react";
 import { AnimateSharedLayout } from "framer-motion";
 import { useMemo } from "react";
 import { IntlProvider } from "react-intl";
 
-export const StoryDecorator: DecoratorFunction<ReactFramework> = function StoryDecorator(
-  story,
-  { globals }
-) {
+export const StoryDecorator: DecoratorFunction = function StoryDecorator(story, { globals }) {
   const messages = useMemo(() => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const data = require(`../lang/${globals.locale}.json`);
