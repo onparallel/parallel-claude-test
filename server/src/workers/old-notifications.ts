a6cb022417a0eabcaf287e74bc680321afb1385b
Santi Albo <santialbo@gmail.com>
2023-02-21 19:04:45 +0100

    [chore] mark notifications as read after 30 days

--- server/src/workers/old-notifications.ts
+++ server/src/workers/old-notifications.ts
@@ -1,7 +1,7 @@
 import { createCronWorker } from "./helpers/createCronWorker";
 
 createCronWorker("old-notifications", async (ctx) => {
-  const MAX_MONTHS = 2;
+  const MAX_MONTHS = 1;
   // mark as read every unread user notifications that was created more than MAX_MONTHS months ago
   await ctx.petitions.markOldPetitionUserNotificationsAsRead(MAX_MONTHS);
 });
