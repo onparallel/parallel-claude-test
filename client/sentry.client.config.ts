b5e80a4795f1171cadb1071b439918f6ef9622e4
Santi Albo <santialbo@gmail.com>
2023-04-25 13:37:56 +0200

    [chore] tweak replaysSessionSampleRate

--- client/sentry.client.config.ts
+++ client/sentry.client.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   debug: false,
   tracesSampleRate: 1,
   replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.01,
   integrations: [
     new Sentry.Replay({
       maskAllText: true,
