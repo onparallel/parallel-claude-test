3940fd65ded0a7138948d867c9f28abc9f16208c
Santi Albo <santialbo@gmail.com>
2023-05-10 11:04:07 +0200

    [chore] remove unnecessary index

--- server/migrations/20230504142127_create-profile_subscription-table.ts
+++ server/migrations/20230504142127_create-profile_subscription-table.ts
@@ -8,9 +8,8 @@ export async function up(knex: Knex): Promise<void> {
     t.integer("user_id").notNullable().references("user.id");
     timestamps(t, { updated: false });
   }).raw(/* sql */ `
-        create unique index profile_subscription__profile_id__user_id on profile_subscription (profile_id, user_id) where deleted_at is null;
-        create index profile_subscription__profile_id on profile_subscription (profile_id) where deleted_at is null;
-      `);
+    create unique index profile_subscription__profile_id__user_id on profile_subscription (profile_id, user_id) where deleted_at is null;
+  `);
 }
 
 export async function down(knex: Knex): Promise<void> {
