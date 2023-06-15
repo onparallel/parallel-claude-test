c0ffbc380419fa6024ee18ee56dbf86e2b8a270b
Santi Albo <santialbo@gmail.com>
2023-06-15 17:19:12 +0200

    [chore] make /status check backend too

--- client/pages/api/status.ts
+++ client/pages/api/status.ts
@@ -1,3 +1,13 @@
-export default function handler(req: any, res: any) {
-  res.status(200).send(process.env.BUILD_ID);
+import fetch from "node-fetch";
+
+export default async function handler(req: any, res: any) {
+  try {
+    const response = await fetch("http://localhost:4000/ping");
+    if (!response.ok) {
+      throw new Error();
+    }
+    res.status(200).send(process.env.BUILD_ID);
+  } catch {
+    res.status(418).send();
+  }
 }
