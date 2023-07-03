68de66d7111353adecd8165865a2fa4aaed977ab
Mariano Rodriguez <mariano.rodriguez.232@gmail.com>
2023-07-03 18:10:14 +0200

    [feat] show "unnamed profile" on activity

--- client/components/common/ProfileLink.tsx
+++ client/components/common/ProfileLink.tsx
@@ -6,7 +6,13 @@ import { FormattedMessage } from "react-intl";
 
 export function ProfileLink({ profile }: { profile?: ProfileLink_ProfileFragment | null }) {
   return profile ? (
-    <Link href={`/app/profiles/${profile.id}`}>{profile.name}</Link>
+    <Link href={`/app/profiles/${profile.id}`}>
+      {profile.name || (
+        <Text textStyle="hint" as="span">
+          <FormattedMessage id="generic.unnamed-profile" defaultMessage="Unnamed profile" />
+        </Text>
+      )}
+    </Link>
   ) : (
     <Text textStyle="hint" as="span">
       <FormattedMessage id="generic.deleted-profile" defaultMessage="Deleted profile" />
