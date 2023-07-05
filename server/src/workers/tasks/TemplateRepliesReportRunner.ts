c6dbb028815289d7698a950bdb663c8e6d731d85
Mariano Rodriguez <mariano@onparallel.com>
2023-07-05 14:57:21 +0200

    [feat] rework TemplateRepliesReportRunner to include all template fields as columns (#1529)
    rework del export de respuestas de una plantilla:
    
    antes de empezar a iterar sobre las respuestas de cada petición se
    construyen los `headers` del excel, compuestos de una serie de columnas
    fijas (parallel-id, parallel-name, recipient-names, etc) y una serie de
    columnas que representa cada campo de la plantilla.
    El orden de las columnas del excel estará definido por el orden de
    inserción de `headers`
    
    Luego, se van construyendo las filas con un `Record<string,
    Maybe<Date|string>>` que representa los valores de cada celda del excel,
    siendo key=el id de la columna definido en los headers y value= el
    contenido de ese campo.
    Si se encuentra un campo desconocido en alguna petición, se añade al
    final del array de headers.
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1204950974461719

--- server/src/workers/tasks/TemplateRepliesReportRunner.ts
+++ server/src/workers/tasks/TemplateRepliesReportRunner.ts
@@ -1,5 +1,6 @@
+import { IntlShape } from "@formatjs/intl";
+import { formatInTimeZone } from "date-fns-tz";
 import Excel from "exceljs";
-import { IntlShape } from "react-intl";
 import { isDefined, minBy, zip } from "remeda";
 import { Readable } from "stream";
 import { PetitionField, PetitionFieldReply, PetitionMessage } from "../../db/__types";
@@ -24,13 +25,20 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
     if (!this.task.user_id) {
       throw new Error(`Task ${this.task.id} is missing user_id`);
     }
+
+    const user = await this.ctx.readonlyUsers.loadUser(this.task.user_id);
+    if (!user) {
+      throw new Error(`User ${this.task.user_id} not found`);
+    }
+
     const hasAccess = await this.ctx.readonlyPetitions.userHasAccessToPetitions(this.task.user_id, [
       templateId,
     ]);
     if (!hasAccess) {
       throw new Error(`User ${this.task.user_id} has no access to petition ${templateId}`);
     }
-    const [template, templateFields, petitions] = await Promise.all([
+    const [includePreviewUrl, template, templateFields, petitions] = await Promise.all([
+      this.ctx.featureFlags.orgHasFeatureFlag(user.org_id, "TEMPLATE_REPLIES_PREVIEW_URL"),
       this.ctx.readonlyPetitions.loadPetition(templateId),
       this.ctx.readonlyPetitions.loadFieldsForPetition(templateId),
       this.ctx.readonlyPetitions.getPetitionsForTemplateRepliesReport(
@@ -39,17 +47,13 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
         endDate
       ),
     ]);
-
-    let rows: Record<
-      string,
-      { position: number; content: Maybe<string | Date>; title: Maybe<string> }
-    >[] = [];
-
     const intl = await this.ctx.i18n.getIntl(template!.recipient_locale);
+
+    const headers = this.buildExcelHeaders(includePreviewUrl, templateFields, intl);
+    let rows: Record<string, Maybe<string | Date>>[] = [];
 
     if (petitions.length > 0) {
       const [
-        includePreviewUrl,
         petitionsAccesses,
         petitionsMessages,
         petitionsFieldsWithReplies,
@@ -57,7 +61,6 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
         petitionsTags,
         petitionsEvents,
       ] = await Promise.all([
-        this.ctx.featureFlags.orgHasFeatureFlag(template!.org_id, "TEMPLATE_REPLIES_PREVIEW_URL"),
         this.ctx.readonlyPetitions.loadAccessesForPetition(petitions.map((p) => p.id)),
         this.ctx.readonlyPetitions.loadMessagesByPetitionId(petitions.map((p) => p.id)),
         this.ctx.readonlyPetitions.getPetitionFieldsWithReplies(petitions.map((p) => p.id)),
@@ -91,9 +94,6 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
         )
       );
 
-      // templateFields are already ordered by position
-      const fieldPositions = templateFields.map((f) => f.id);
-
       rows = petitions.map((petition, petitionIndex) => {
         const petitionFields = petitionsFieldsWithReplies[petitionIndex];
 
@@ -105,24 +105,6 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
 
         const contactNames = contacts.map((c) => fullName(c.first_name, c.last_name));
         const contactEmails = contacts.map((c) => c.email);
-
-        const dateParts = firstSendDate
-          ? intl.formatDateToParts(firstSendDate, {
-              timeZone: timezone,
-              day: "numeric",
-              month: "numeric",
-              year: "numeric",
-              hour: "numeric",
-              minute: "numeric",
-              hour12: false,
-            })
-          : [];
-
-        const year = dateParts.find((p) => p.type === "year")!;
-        const month = dateParts.find((p) => p.type === "month")!;
-        const day = dateParts.find((p) => p.type === "day")!;
-        const hour = dateParts.find((p) => p.type === "hour")!;
-        const minute = dateParts.find((p) => p.type === "minute")!;
 
         const petitionOwner = petitionsOwnerUserData[petitionIndex];
         const petitionTags = petitionsTags[petitionIndex];
@@ -137,184 +119,87 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
           .filter((e) => e.type === "SIGNATURE_COMPLETED")
           .at(-1);
 
-        const row: Record<
-          string,
-          { position: number; content: Maybe<string | Date>; title: Maybe<string> }
-        > = {
-          "parallel-id": {
-            position: 0,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.petition-id",
-              defaultMessage: "Parallel ID",
-            }),
-            content: toGlobalId("Petition", petition.id),
-          },
-          "parallel-name": {
-            position: 1,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.petition-name",
-              defaultMessage: "Parallel name",
-            }),
-            content: petition.name || "",
-          },
-          "recipient-names": {
-            position: 2,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.recipient-names",
-              defaultMessage: "Recipient names",
-            }),
-            content: contactNames.join(", ") || "",
-          },
-          "recipient-emails": {
-            position: 3,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.recipient-emails",
-              defaultMessage: "Recipient emails",
-            }),
-            content: contactEmails.join(", ") || "",
-          },
-          "send-date": {
-            position: 4,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.sent-at",
-              defaultMessage: "Sent at",
-            }),
-            content: firstSendDate
-              ? new Date(
-                  Date.UTC(
-                    parseInt(year.value),
-                    parseInt(month.value) - 1, // months go from 0 (Jan) to 11 (Dec)
-                    parseInt(day.value),
-                    parseInt(hour.value),
-                    parseInt(minute.value)
-                  )
-                )
-              : null,
-          },
-          "sent-by": {
-            position: 5,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.sent-by",
-              defaultMessage: "Sent by",
-            }),
-            content: fullName(
-              petitionFirstMessageUserData?.first_name,
-              petitionFirstMessageUserData?.last_name
-            ),
-          },
-          "parallel-owner": {
-            position: 6,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-owner",
-              defaultMessage: "Owner",
-            }),
-            content: fullName(petitionOwner?.first_name, petitionOwner?.last_name),
-          },
-          tags: {
-            position: 7,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-tags",
-              defaultMessage: "Tags",
-            }),
-            content: petitionTags.map((t) => t.name).join(", "),
-          },
-          status: {
-            position: 8,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-status",
-              defaultMessage: "Status",
-            }),
-            content: petition.status
-              ? {
-                  DRAFT: intl.formatMessage({
-                    id: "export-template-report.petition-status.draft",
-                    defaultMessage: "DRAFT",
-                  }),
-                  PENDING: intl.formatMessage({
-                    id: "export-template-report.petition-status.pending",
-                    defaultMessage: "PENDING",
-                  }),
-                  COMPLETED: intl.formatMessage({
-                    id: "export-template-report.petition-status.completed",
-                    defaultMessage: "COMPLETED",
-                  }),
-                  CLOSED: intl.formatMessage({
-                    id: "export-template-report.petition-status.closed",
-                    defaultMessage: "CLOSED",
-                  }),
-                }[petition.status]
-              : null,
-          },
-          "completed-at": {
-            position: 9,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-completed-at",
-              defaultMessage: "Completed at",
-            }),
-            content: latestPetitionCompletedEvent?.created_at ?? null,
-          },
-          "closed-at": {
-            position: 10,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-closed-at",
-              defaultMessage: "Closed at",
-            }),
-            content: latestPetitionClosedEvent?.created_at ?? null,
-          },
-          "signed-at": {
-            position: 11,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.parallel-signed-at",
-              defaultMessage: "Signed at",
-            }),
-            content: latestSignatureCompletedEvent?.created_at ?? null,
-          },
+        const row: Record<string, Maybe<string | Date>> = {
+          "parallel-id": toGlobalId("Petition", petition.id),
+          "parallel-name": petition.name || "",
+          "recipient-names": contactNames.join(", ") || "",
+          "recipient-emails": contactEmails.join(", ") || "",
+          "send-date": firstSendDate
+            ? new Date(formatInTimeZone(firstSendDate, timezone, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+            : null,
+          "sent-by": fullName(
+            petitionFirstMessageUserData?.first_name,
+            petitionFirstMessageUserData?.last_name
+          ),
+          "parallel-owner": fullName(petitionOwner?.first_name, petitionOwner?.last_name),
+          tags: petitionTags.map((t) => t.name).join(", "),
+          status: petition.status
+            ? {
+                DRAFT: intl.formatMessage({
+                  id: "export-template-report.petition-status.draft",
+                  defaultMessage: "DRAFT",
+                }),
+                PENDING: intl.formatMessage({
+                  id: "export-template-report.petition-status.pending",
+                  defaultMessage: "PENDING",
+                }),
+                COMPLETED: intl.formatMessage({
+                  id: "export-template-report.petition-status.completed",
+                  defaultMessage: "COMPLETED",
+                }),
+                CLOSED: intl.formatMessage({
+                  id: "export-template-report.petition-status.closed",
+                  defaultMessage: "CLOSED",
+                }),
+              }[petition.status]
+            : null,
+          "completed-at": latestPetitionCompletedEvent?.created_at ?? null,
+          "closed-at": latestPetitionClosedEvent?.created_at ?? null,
+          "signed-at": latestSignatureCompletedEvent?.created_at ?? null,
         };
 
         if (includePreviewUrl) {
-          row["preview-url"] = {
-            position: Object.keys(row).length,
-            title: intl.formatMessage({
-              id: "export-template-report.column-header.preview-url",
-              defaultMessage: "Preview URL",
-            }),
-            content: `${this.ctx.config.misc.parallelUrl}/${intl.locale}/app/petitions/${toGlobalId(
-              "Petition",
-              petition.id
-            )}/preview`,
-          };
+          row["preview-url"] = `${this.ctx.config.misc.parallelUrl}/${
+            intl.locale
+          }/app/petitions/${toGlobalId("Petition", petition.id)}/preview`;
         }
-        const fixedColsLength = Object.keys(row).length;
 
         const fieldIndexes = getFieldIndices(petitionFields);
         const visibilities = evaluateFieldVisibility(petitionFields);
         zip(petitionFields, fieldIndexes)
           .filter((_, i) => visibilities[i])
           .forEach(([field]) => {
-            row[`${field.from_petition_field_id ?? field.id}`] = {
-              position: this.getFieldPosition(field, fieldPositions) + fixedColsLength,
-              title: field.type === "DATE_TIME" ? field.title + " (UTC)" : field.title,
-              content: !isFileTypeField(field.type)
-                ? field.replies
-                    .map((r) => this.replyContent({ ...r, type: field.type }, intl as IntlShape))
-                    .join("; ")
-                : field.replies.length > 0
-                ? intl.formatMessage(
-                    {
-                      id: "export-template-report.file-cell-content",
-                      defaultMessage: "{count, plural, =1{1 file} other {# files}}",
-                    },
-                    { count: field.replies.length }
-                  )
-                : "",
-            };
+            const columnId = `field-${field.from_petition_field_id ?? field.id}`;
+
+            // make sure header is defined on this field
+            const header = headers.find((h) => h.id === columnId);
+            if (!header) {
+              headers.push({
+                id: columnId,
+                title: (field.type === "DATE_TIME" ? field.title + " (UTC)" : field.title) ?? "",
+              });
+            }
+
+            row[columnId] = !isFileTypeField(field.type)
+              ? field.replies
+                  .map((r) => this.replyContent({ ...r, type: field.type }, intl as IntlShape))
+                  .join("; ")
+              : field.replies.length > 0
+              ? intl.formatMessage(
+                  {
+                    id: "export-template-report.file-cell-content",
+                    defaultMessage: "{count, plural, =1{1 file} other {# files}}",
+                  },
+                  { count: field.replies.length }
+                )
+              : "";
           });
 
         return row;
       });
     }
 
-    const stream = await this.exportToExcel(rows);
+    const stream = await this.exportToExcel(headers, rows);
 
     const tmpFile = await this.uploadTemporaryFile({
       stream,
@@ -329,24 +214,6 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
     });
 
     return { temporary_file_id: tmpFile.id };
-  }
-
-  private getFieldPosition(
-    field: Pick<PetitionField, "id" | "from_petition_field_id">,
-    orderArray: number[]
-  ) {
-    if (isDefined(field.from_petition_field_id)) {
-      const position = orderArray.indexOf(field.from_petition_field_id);
-      if (position === -1) {
-        orderArray.push(field.from_petition_field_id);
-        return orderArray.length - 1;
-      } else {
-        return position;
-      }
-    } else {
-      orderArray.push(field.id);
-      return orderArray.length - 1;
-    }
   }
 
   private replyContent(r: Pick<PetitionFieldReply, "content" | "type">, intl: IntlShape) {
@@ -369,47 +236,130 @@ export class TemplateRepliesReportRunner extends TaskRunner<"TEMPLATE_REPLIES_RE
   }
 
   private async exportToExcel(
-    rows: Record<
-      string,
-      { position: number; content: Maybe<string | Date>; title: Maybe<string> }
-    >[]
+    headers: { id: string; title: string }[],
+    rows: Record<string, Maybe<string | Date>>[]
   ) {
     const wb = new Excel.Workbook();
-
     const page = wb.addWorksheet();
-    const headers: { key: string; data: { position: number; title: Maybe<string> } }[] = [];
-    rows.forEach((row) => {
-      Object.entries(row).forEach(([key, data]) => {
-        const pushedKey = headers.find((h) => h.key === key);
-        if (!pushedKey) {
-          headers.push({ key, data });
-          // if the same field is found on another position, keep only the first one to avoid duplicating columns
-        } else if (pushedKey.data.position > data.position) {
-          pushedKey.data = data;
-        }
-      });
-    });
-
-    headers.sort((a, b) => a.data.position - b.data.position);
 
     page.columns = headers.map((h) => ({
-      key: h.key,
-      header: h.data.title ?? "",
+      key: h.id,
+      header: h.title,
     }));
 
-    page.addRows(
-      rows.map((row) => {
-        const obj: any = {};
-        Object.entries(row).forEach(([key, data]) => {
-          obj[key] = data.content;
-        });
-        return obj;
-      })
-    );
+    page.addRows(rows);
 
     const stream = new Readable();
     stream.push(await wb.xlsx.writeBuffer());
     stream.push(null); // end of stream
     return stream;
+  }
+
+  private buildExcelHeaders(includePreviewUrl: boolean, fields: PetitionField[], intl: IntlShape) {
+    const headers = [
+      {
+        id: "parallel-id",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.petition-id",
+          defaultMessage: "Parallel ID",
+        }),
+      },
+      {
+        id: "parallel-name",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.petition-name",
+          defaultMessage: "Parallel name",
+        }),
+      },
+      {
+        id: "recipient-names",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.recipient-names",
+          defaultMessage: "Recipient names",
+        }),
+      },
+      {
+        id: "recipient-emails",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.recipient-emails",
+          defaultMessage: "Recipient emails",
+        }),
+      },
+      {
+        id: "send-date",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.sent-at",
+          defaultMessage: "Sent at",
+        }),
+      },
+      {
+        id: "sent-by",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.sent-by",
+          defaultMessage: "Sent by",
+        }),
+      },
+      {
+        id: "parallel-owner",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-owner",
+          defaultMessage: "Owner",
+        }),
+      },
+      {
+        id: "tags",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-tags",
+          defaultMessage: "Tags",
+        }),
+      },
+      {
+        id: "status",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-status",
+          defaultMessage: "Status",
+        }),
+      },
+      {
+        id: "completed-at",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-completed-at",
+          defaultMessage: "Completed at",
+        }),
+      },
+      {
+        id: "closed-at",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-closed-at",
+          defaultMessage: "Closed at",
+        }),
+      },
+      {
+        id: "signed-at",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.parallel-signed-at",
+          defaultMessage: "Signed at",
+        }),
+      },
+    ];
+
+    if (includePreviewUrl) {
+      headers.push({
+        id: "preview-url",
+        title: intl.formatMessage({
+          id: "export-template-report.column-header.preview-url",
+          defaultMessage: "Preview URL",
+        }),
+      });
+    }
+
+    for (const field of fields.filter((f) => f.type !== "HEADING")) {
+      headers.push({
+        id: `field-${field.id}`,
+        title: (field.type === "DATE_TIME" ? field.title + " (UTC)" : field.title) ?? "",
+      });
+    }
+
+    return headers;
   }
 }
