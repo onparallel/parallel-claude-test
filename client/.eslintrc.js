9d1f0bb6d434513e5a87bc164a0bc2076900c131
Santi Albo <santialbo@gmail.com>
2023-08-29 10:27:32 +0200

    [chore] warn on incorrect i18n ids

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   rules: {
     "no-console": ["error", { allow: ["warn", "error"] }],
+    "formatjs/enforce-id": [
+      "warn",
+      {
+        idWhitelist: ["^((component|page)\\.[a-z-]+\\.[a-z-]+|(generic)\\.[a-z-]+)$"],
+        idInterpolationPattern: "*",
+      },
+    ],
   },
 };
