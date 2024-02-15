1a616d910f2cbc20a8958a28c7da6872b4ca0324
Mariano Rodriguez <mariano@onparallel.com>
2024-02-15 19:35:57 +0100

    [feat] add hardcoded signature for Aliseda (#1855)
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1206602760188408

--- server/src/pdf/components/SignaturesBlock.tsx
+++ server/src/pdf/components/SignaturesBlock.tsx
@@ -39,135 +39,135 @@ export function SignaturesBlock({ signatureConfig, templateId }: SignaturesBlock
     ...FORMATS.L,
   });
 
-  const signers: SignatureBoxProps[] = [
-    ...(process.env.NODE_ENV === "production" && isDefined(templateId)
-      ? [
-          "EAwW2jXkP4C9LjU2b3",
-          "EAwW2jXkP4C9LjU2fS",
-          "EAwW2jXkP4C9Lf4DDK",
-          "zas25KHxAByKWUZSNGc",
-        ].includes(templateId)
-        ? [
-            {
-              fullName: "Guillermo Preckler",
-              signatureImageUrl:
-                "https://static.onparallel.com/static/images/signatures/guillermo-preckler-brickbro.png",
-              date,
-            },
-          ]
-        : templateId === "EAwW2jXkP4C9LbfNRp"
-          ? [
-              {
-                fullName: "TIKO",
-                signatureImageUrl:
-                  "https://static.onparallel.com/static/images/signatures/sello-tiko.png",
-                date,
-              },
-            ]
-          : ["zas25KHxAByKWUgG8U2", "zas25KHxAByKWUhxeC6"].includes(templateId)
-            ? [
-                {
-                  fullName: "Marta Blanco Maseda",
-                  signatureImageUrl:
-                    "https://static.onparallel.com/static/images/signatures/marta-blanco-maseda-imasd.png",
-                  date,
-                },
-              ]
-            : [
-                  "zas25KHxAByKWmEFbpV",
-                  "zas25KHxAByKWmEFbpU",
-                  "zas25KHxAByKWmEFbpT",
-                  "zas25KHxAByKWmM6rrm",
-                ].includes(templateId)
-              ? [
-                  {
-                    fullName: "Carlos Guerrero Martín",
-                    signatureImageUrl:
-                      "https://static.onparallel.com/static/images/signatures/carlos-guerrero-martin-debify.png",
-                    date,
-                  },
-                ]
-              : [
-                    "zas25KHxAByKWmFx9gf",
-                    "zas25KHxAByKWmHefVF",
-                    "zas25KHxAByKWmHefVH",
-                    "zas25KHxAByKWmHefVK",
-                    "zas25KHxAByKWmHefZj",
-                    "zas25KHxAByKWmHefZm",
-                    "zas25KHxAByKWmHguoJ",
-                    "zas25KHxAByKWmHgusw",
-                    "zas25KHxAByKWmHguxV",
-                    "zas25KHxAByKX3mJq5E",
-                    "zas25KHxAByKX3usjzq",
-                    "zas25KHxAByKXKL5aMA",
-                    "zas25KHxAByKXKJPAzn",
-                    "zas25KHxAByKXBVeaGh",
-                  ].includes(templateId)
-                ? [
-                    {
-                      fullName: "Jorge Arturo Cáceres Quezada",
-                      signatureImageUrl:
-                        "https://static.onparallel.com/static/images/signatures/jorge-arturo-caceres-rive.png",
-                      date,
-                    },
-                  ]
-                : ["zas25KHxAByKWmKNDXb", "zas25KHxAByKWmKRZgb"].includes(templateId)
-                  ? [
-                      {
-                        fullName: "Dña Ana Ortiz Canela",
-                        signatureImageUrl:
-                          "https://static.onparallel.com/static/images/signatures/ana-ortiz-canela-gesdocument.png",
-                        date,
-                      },
-                    ]
-                  : ["zas25KHxAByKWu4g76M"].includes(templateId)
-                    ? [
-                        {
-                          fullName: "Fernando López Clemente",
-                          signatureImageUrl:
-                            "https://static.onparallel.com/static/images/signatures/fernando-lopez-clemente-anticipa.png",
-                          date,
-                        },
-                      ]
-                    : ["zas25KHxAByKXKJNo2z"].includes(templateId)
-                      ? [
-                          {
-                            fullName: "D. Álvaro Gámez Serracarbassa",
-                            signatureImageUrl:
-                              "https://static.onparallel.com/static/images/signatures/alvaro-gamez-serracarbassa-broseta.png",
-                            date,
-                          },
-                          {
-                            fullName: "D. Claudio Aguiló Casanova",
-                            signatureImageUrl:
-                              "https://static.onparallel.com/static/images/signatures/claudio-aguilo-casanova-broseta.png",
-                            date,
-                          },
-                        ]
-                      : ["zas25KHxAByKXKRCYU4", "zas25KHxAByKXKUbz48"].includes(templateId)
-                        ? [
-                            {
-                              fullName: "Juan Francisco Sahuquillo Cebrián",
-                              signatureImageUrl:
-                                "https://static.onparallel.com/static/images/signatures/juan-francisco-sahuquillo-cebrian-tiko.png",
-                            },
-                          ]
-                        : ["zas25KHxAByKXaxCQoK"].includes(templateId)
-                          ? [
-                              {
-                                fullName: "Manuel Holstein",
-                                signatureImageUrl:
-                                  "https://static.onparallel.com/static/images/signatures/manuel-holstein-tiko.png",
-                              },
-                            ]
-                          : []
-      : []),
+  const signers: SignatureBoxProps[] = [];
+
+  if (process.env.NODE_ENV === "production" && isDefined(templateId)) {
+    if (
+      [
+        "EAwW2jXkP4C9LjU2b3",
+        "EAwW2jXkP4C9LjU2fS",
+        "EAwW2jXkP4C9Lf4DDK",
+        "zas25KHxAByKWUZSNGc",
+      ].includes(templateId)
+    ) {
+      signers.push({
+        fullName: "Guillermo Preckler",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/guillermo-preckler-brickbro.png",
+        date,
+      });
+    } else if (templateId === "EAwW2jXkP4C9LbfNRp") {
+      signers.push({
+        fullName: "TIKO",
+        signatureImageUrl: "https://static.onparallel.com/static/images/signatures/sello-tiko.png",
+        date,
+      });
+    } else if (["zas25KHxAByKWUgG8U2", "zas25KHxAByKWUhxeC6"].includes(templateId)) {
+      signers.push({
+        fullName: "Marta Blanco Maseda",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/marta-blanco-maseda-imasd.png",
+        date,
+      });
+    } else if (
+      [
+        "zas25KHxAByKWmEFbpV",
+        "zas25KHxAByKWmEFbpU",
+        "zas25KHxAByKWmEFbpT",
+        "zas25KHxAByKWmM6rrm",
+      ].includes(templateId)
+    ) {
+      signers.push({
+        fullName: "Carlos Guerrero Martín",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/carlos-guerrero-martin-debify.png",
+        date,
+      });
+    } else if (
+      [
+        "zas25KHxAByKWmFx9gf",
+        "zas25KHxAByKWmHefVF",
+        "zas25KHxAByKWmHefVH",
+        "zas25KHxAByKWmHefVK",
+        "zas25KHxAByKWmHefZj",
+        "zas25KHxAByKWmHefZm",
+        "zas25KHxAByKWmHguoJ",
+        "zas25KHxAByKWmHgusw",
+        "zas25KHxAByKWmHguxV",
+        "zas25KHxAByKX3mJq5E",
+        "zas25KHxAByKX3usjzq",
+        "zas25KHxAByKXKL5aMA",
+        "zas25KHxAByKXKJPAzn",
+        "zas25KHxAByKXBVeaGh",
+      ].includes(templateId)
+    ) {
+      signers.push({
+        fullName: "Jorge Arturo Cáceres Quezada",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/jorge-arturo-caceres-rive.png",
+        date,
+      });
+    } else if (["zas25KHxAByKWmKNDXb", "zas25KHxAByKWmKRZgb"].includes(templateId)) {
+      signers.push({
+        fullName: "Dña Ana Ortiz Canela",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/ana-ortiz-canela-gesdocument.png",
+        date,
+      });
+    } else if (["zas25KHxAByKWu4g76M"].includes(templateId)) {
+      signers.push({
+        fullName: "Fernando López Clemente",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/fernando-lopez-clemente-anticipa.png",
+        date,
+      });
+    } else if (["zas25KHxAByKXKJNo2z"].includes(templateId)) {
+      signers.push(
+        {
+          fullName: "D. Álvaro Gámez Serracarbassa",
+          signatureImageUrl:
+            "https://static.onparallel.com/static/images/signatures/alvaro-gamez-serracarbassa-broseta.png",
+          date,
+        },
+        {
+          fullName: "D. Claudio Aguiló Casanova",
+          signatureImageUrl:
+            "https://static.onparallel.com/static/images/signatures/claudio-aguilo-casanova-broseta.png",
+          date,
+        },
+      );
+    } else if (["zas25KHxAByKXKRCYU4", "zas25KHxAByKXKUbz48"].includes(templateId)) {
+      signers.push({
+        fullName: "Juan Francisco Sahuquillo Cebrián",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/juan-francisco-sahuquillo-cebrian-tiko.png",
+        date,
+      });
+    } else if (["zas25KHxAByKXaxCQoK"].includes(templateId)) {
+      signers.push({
+        fullName: "Manuel Holstein",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/manuel-holstein-tiko.png",
+        date,
+      });
+    } else if (["zas25KHxAByKX3yGfeP", "zas25KHxAByKXBXLcU6"].includes(templateId)) {
+      signers.push({
+        fullName: "Miguel Acosta",
+        signatureImageUrl:
+          "https://static.onparallel.com/static/images/signatures/miguel-acosta-aliseda.png",
+        date,
+      });
+    }
+  }
+
+  signers.push(
     ...signatureConfig.signers!.map((signer, i) => ({
       wordAnchor: `3cb39pzCQA9wJ${i}`,
       fullName: signer!.fullName,
       date,
     })),
-  ];
+  );
+
   const legalText = theme.legalText[intl.locale as ContactLocale];
   return (
     <View
