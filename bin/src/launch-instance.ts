209e445a6c5c48ea8e5c75283a153ee3fb924527
Santi Albo <santialbo@gmail.com>
2024-04-29 16:04:29 +0200

    [chore] update ami

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -28,7 +28,7 @@ const INSTANCE_TYPES = {
   staging: "t3.medium",
 } satisfies Record<Environment, _InstanceType>;
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0211ec7df6ff4f352";
+const IMAGE_ID = "ami-06327e319893f8e4a";
 const KMS_KEY_ID = "acf1d245-abe5-4ff8-a490-09dba3834c45";
 const SECURITY_GROUP_IDS = {
   production: ["sg-078abc8a772035e7a"],
