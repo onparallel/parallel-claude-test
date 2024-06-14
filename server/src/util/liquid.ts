66f42932b4d097c72f9b20f16e848128c1c84a14
Santi Albo <santialbo@gmail.com>
2024-06-14 12:16:46 +0200

    [feat] add roman and letter filters (#2040)
    <img width="771" alt="image"
    src="https://github.com/onparallel/parallel/assets/1557563/6fd0778f-c20b-45a4-a40d-0ea8a72e5c12">
    
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1207559335595132

--- server/src/util/liquid.ts
+++ server/src/util/liquid.ts
@@ -24,6 +24,75 @@ export function createLiquid() {
     });
   });
 
+  engine.registerFilter("letter", function (value: any, digits?: number) {
+    if (typeof value === "string") {
+      value = parseFloat(value);
+    } else if (typeof value === "object" && "valueOf" in value) {
+      value = parseFloat(value.valueOf());
+    }
+    if (typeof value !== "number" || value < 0) {
+      return undefined;
+    }
+    let letters = "";
+    while (value >= 1) {
+      const remainder = (value - 1) % 26;
+      letters = String.fromCharCode(65 + remainder) + letters;
+      value = Math.floor((value - 1) / 26);
+    }
+    return letters;
+  });
+
+  engine.registerFilter("roman", function (value: any, digits?: number) {
+    if (typeof value === "string") {
+      value = parseFloat(value);
+    } else if (typeof value === "object" && "valueOf" in value) {
+      value = parseFloat(value.valueOf());
+    }
+    if (typeof value !== "number" || value <= 0 || value >= 4000000) {
+      return undefined;
+    }
+
+    const val = [
+      1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000,
+      900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1,
+    ];
+    const syms = [
+      "_M",
+      "_C_M",
+      "_D",
+      "_C_D",
+      "_C",
+      "_X_C",
+      "_L",
+      "_X_L",
+      "_X",
+      "_I_X",
+      "_V",
+      "_I_V",
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I",
+    ];
+    let roman = "";
+    for (let i = 0; i < val.length; i++) {
+      while (value >= val[i]) {
+        value -= val[i];
+        roman += syms[i];
+      }
+    }
+    return roman.replace(/_(.)/g, "$1\u0305");
+  });
+
   engine.registerFilter("currency", function (value: any, currency: string) {
     if (typeof value === "string") {
       value = parseFloat(value);
