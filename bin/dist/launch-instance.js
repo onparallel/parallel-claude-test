0f41e662a5efa51ff8ebc8a2f3c7d72565452f06
Santi Albo <santialbo@gmail.com>
2024-07-29 12:36:57 +0200

    [chore] update ami

--- bin/dist/launch-instance.js
+++ bin/dist/launch-instance.js
@@ -17,7 +17,7 @@ const INSTANCE_TYPES = {
     staging: "t3.medium",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0eaba6464f3daccf5";
+const IMAGE_ID = "ami-0eda2f995ddaa7670";
 const KMS_KEY_ID = "acf1d245-abe5-4ff8-a490-09dba3834c45";
 const SECURITY_GROUP_IDS = {
     production: ["sg-078abc8a772035e7a"],
