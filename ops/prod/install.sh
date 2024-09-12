17a21bd7f2cbb1ac3e7d2626d9f1c7c8442047eb
Santi Albo <santialbo@gmail.com>
2024-09-12 18:04:52 +0200

    [fix] enable task worker so it restarts after reboots

--- ops/prod/install.sh
+++ ops/prod/install.sh
@@ -47,6 +47,7 @@ sudo systemctl enable parallel-email-events-queue.service
 sudo systemctl enable parallel-email-sender-queue.service
 sudo systemctl enable parallel-event-processor-queue.service
 sudo systemctl enable parallel-signature-worker-queue.service
+sudo systemctl enable parallel-task-worker-queue.service
 if [[ "$ENV" == "production" ]]; then
   sudo systemctl enable parallel-task-worker-queue\@{1..3}.service
 else
