3fee746fe4647ff670c538d9db07fc64b50aaa81
Santi Albo <santialbo@gmail.com>
2024-11-06 13:52:43 +0100

    [chore] update logo name

--- server/src/graphql/integrations/types.ts
+++ server/src/graphql/integrations/types.ts
@@ -49,7 +49,7 @@ export const IOrgIntegration = interfaceType({
           DOCUSIGN: "docusign.png",
           SIGNATURIT: "signaturit.png",
           EINFORMA: "einforma.png",
-          COMPANIES_HOUSE: "companies-house.png",
+          COMPANIES_HOUSE: "gov-uk.png",
         }[root.provider];
         return isNonNullish(asset)
           ? await ctx.images.getAssetImageUrl(`static/logos/${asset}`, args.options as any)
