1fdf550d8a9c7c78a1478179134535ad76f0cb31
Santi Albo <santialbo@gmail.com>
2024-12-20 10:16:33 +0100

    [chore] rever to previous ami

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -28,7 +28,7 @@ const INSTANCE_TYPES = {
   staging: "t3.medium",
 } satisfies Record<Environment, _InstanceType>;
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-0ba3d54424de79881";
+const IMAGE_ID = "ami-0c8d7736543d8f92e";
 const KMS_KEY_ID = "acf1d245-abe5-4ff8-a490-09dba3834c45";
 const SECURITY_GROUP_IDS = {
   production: ["sg-078abc8a772035e7a"],
