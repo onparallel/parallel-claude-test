0b403d8659570b88e4a3dcd21769ccf599d7b9c5
Santi Albo <santialbo@gmail.com>
2025-03-25 14:35:05 +0100

    [chore] fix pdfjs on development

---
+++ client/public/pdf.worker.development.mjs
@@ -0,0 +1,1 @@
+await import("/static/js/pdf.worker.min.mjs");
--- client/utils/pdfjs.ts
+++ client/utils/pdfjs.ts
@@ -2,8 +2,9 @@ export type PdfJs = Awaited<typeof import("pdfjs-dist")>;
 
 export async function loadPdfJs(): Promise<PdfJs> {
   const pdfjs = await import("pdfjs-dist");
-  // copy from ../node_modules/pdfjs-dist/legacy/build/
-  pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker${process.env.NEXT_PUBLIC_ENVIRONMENT === "staging" ? ".staging" : ""}.mjs`;
-
+  pdfjs.GlobalWorkerOptions.workerSrc =
+    `/pdf.worker` + process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+      ? ".mjs"
+      : `.${process.env.NEXT_PUBLIC_ENVIRONMENT}.mjs`;
   return pdfjs;
 }
