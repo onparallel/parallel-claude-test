5c3d1872184ef440c4f231c5044eb3a5ce1b4b60
Santi Albo <santialbo@gmail.com>
2025-03-25 15:27:05 +0100

    [fix] fix pdfjs script

--- client/utils/pdfjs.ts
+++ client/utils/pdfjs.ts
@@ -3,8 +3,9 @@ export type PdfJs = Awaited<typeof import("pdfjs-dist")>;
 export async function loadPdfJs(): Promise<PdfJs> {
   const pdfjs = await import("pdfjs-dist");
   pdfjs.GlobalWorkerOptions.workerSrc =
-    `/pdf.worker` + process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+    `/pdf.worker` +
+    (process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
       ? ".mjs"
-      : `.${process.env.NEXT_PUBLIC_ENVIRONMENT}.mjs`;
+      : `.${process.env.NEXT_PUBLIC_ENVIRONMENT}.mjs`);
   return pdfjs;
 }
