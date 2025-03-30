cdfe260c5aabed6a9fc6157bd27e9c133c9ecead
Santi Albo <santialbo@gmail.com>
2025-03-30 09:19:59 +0200

    [chore] update image

--- bin/dist/launch-instance.js
+++ bin/dist/launch-instance.js
@@ -18,7 +18,7 @@ const INSTANCE_TYPES = {
     staging: "t3.large",
 };
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-07acb4da058299d07";
+const IMAGE_ID = "ami-02eacf2ab3b98b67a";
 const KMS_KEY_ID = "acf1d245-abe5-4ff8-a490-09dba3834c45";
 const SECURITY_GROUP_IDS = {
     production: ["sg-078abc8a772035e7a"],
