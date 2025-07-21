c3dac5ffd317209eeed4216acf325609037fddbc
Konstantin Klykov Lazovskaia <kklykov@gmail.com>
2025-07-21 17:01:24 +0200

    [fix] format partial date (#2599)
    <img width="821" height="217" alt="image"
    src="https://github.com/user-attachments/assets/7ddc5e75-f6f7-465b-ac12-3fc1b872d738"
    />

--- client/utils/formatPartialDate.ts
+++ client/utils/formatPartialDate.ts
@@ -4,18 +4,25 @@ import { useIntl } from "react-intl";
 // Same function in server/src/pdf/utils/formatPartialDate.ts to format the date in the PDF
 // date is a string in the format "YYYY-MM-DD" or "YYYY-MM" or "YYYY"
 
-export function formatPartialDate({ date, separator = "-" }: { date: string; separator?: string }) {
+export function formatPartialDate({ date }: { date: string }) {
   const intl = useIntl();
-  const splittedDate = date.split(separator).map((d) => parseInt(d));
+  const splittedDate = date.split("-").map((d) => parseInt(d));
 
   if (splittedDate.length === 1) {
-    return date;
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
+      year: "numeric",
+    });
   } else if (splittedDate.length === 2) {
-    return intl.formatDate(new Date(splittedDate[0], splittedDate[1], 1), {
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
       month: "long",
       year: "numeric",
     });
   } else {
-    return intl.formatDate(new Date(splittedDate[0], splittedDate[1], splittedDate[2]), FORMATS.ll);
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
+      ...FORMATS.ll,
+    });
   }
 }
--- server/src/pdf/utils/formatPartialDate.ts
+++ server/src/pdf/utils/formatPartialDate.ts
@@ -1,25 +1,24 @@
 import { IntlShape } from "react-intl";
 import { FORMATS } from "../../util/dates";
 
-export function formatPartialDate({
-  date,
-  separator = "-",
-  intl,
-}: {
-  date: string;
-  separator?: string;
-  intl: IntlShape;
-}) {
-  const splittedDate = date.split(separator).map((d) => parseInt(d));
+export function formatPartialDate({ date, intl }: { date: string; intl: IntlShape }) {
+  const splittedDate = date.split("-").map((d) => parseInt(d));
 
   if (splittedDate.length === 1) {
-    return date;
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
+      year: "numeric",
+    });
   } else if (splittedDate.length === 2) {
-    return intl.formatDate(new Date(splittedDate[0], splittedDate[1], 1), {
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
       month: "long",
       year: "numeric",
     });
   } else {
-    return intl.formatDate(new Date(splittedDate[0], splittedDate[1], splittedDate[2]), FORMATS.ll);
+    return intl.formatDate(new Date(date), {
+      timeZone: "UTC",
+      ...FORMATS.ll,
+    });
   }
 }
