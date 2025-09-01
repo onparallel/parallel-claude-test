cdcef22665833be30bd9bf06de1c8634cc0e25af
Santi Albo <santialbo@gmail.com>
2025-09-01 14:41:07 +0200

    [chore] remove unnecessary constraints

--- server/migrations/20250806133626_add-is_unique-to-profile-type-field.ts
+++ server/migrations/20250806133626_add-is_unique-to-profile-type-field.ts
@@ -10,20 +10,6 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(/* sql */ `
     create unique index profile_field_value__unique_values_uniq on profile_field_value (profile_type_field_id, (content->>'value'))
     where profile_type_field_is_unique and removed_at is null and deleted_at is null;
-
-    alter table profile_field_value
-    add constraint profile_field_value__is_unique__type__check
-    check (
-      (profile_type_field_is_unique and type = 'SHORT_TEXT')
-      or not profile_type_field_is_unique
-    );
-
-    alter table profile_type_field
-    add constraint profile_type_field__is_unique__type__check
-    check (
-      (is_unique and type = 'SHORT_TEXT')
-      or not is_unique
-    );
   `);
 }
 
