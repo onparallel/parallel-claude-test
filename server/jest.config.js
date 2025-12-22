45a53955bd768f815050b8eb0b13ebf4912e4620
Santi Albo <santialbo@gmail.com>
2025-12-22 13:08:22 +0100

    [chore] rename globalSetup

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: "node",
   setupFiles: ["./test/setupFile.ts"],
   setupFilesAfterEnv: ["./test/setupTests.ts"],
-  globalSetup: "./test/setup.ts",
+  globalSetup: "./test/globalSetup.ts",
   testMatch: ["**/__tests__/**/*.test.ts?(x)"],
   testTimeout: 12000,
   coveragePathIgnorePatterns: ["/node_modules/", "/src/emails/", "src/pdf/"],
