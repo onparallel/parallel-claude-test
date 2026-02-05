ce925f0f47485184d38f16a88f0b3d6ca4a65717
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2026-02-05 14:07:54 +0100

    [chore] update ami

--- bin/src/launch-instance.ts
+++ bin/src/launch-instance.ts
@@ -26,7 +26,7 @@ const INSTANCE_TYPES = {
   staging: "t3.large",
 } satisfies Record<Environment, _InstanceType>;
 const KEY_NAME = "ops";
-const IMAGE_ID = "ami-05007ef0b2c08abde";
+const IMAGE_ID = "ami-0ecea2054f7ec7dfd";
 const KMS_KEY_ID = "acf1d245-abe5-4ff8-a490-09dba3834c45";
 const SECURITY_GROUP_IDS = {
   production: ["sg-078abc8a772035e7a"],
