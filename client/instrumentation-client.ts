9d69090ddbe1dcd80fd436d737a42b7630cef9b8
Santi Albo <santialbo@gmail.com>
2026-02-12 10:56:28 +0100

    [chore] fix sentry config

--- client/instrumentation-client.ts
+++ client/instrumentation-client.ts
@@ -5,16 +5,40 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://9b8d902a0e064afeb5e6c1c45086aea1@o488034.ingest.us.sentry.io/5547679",
-
+  enabled: process.env.NODE_ENV === "production",
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  release: process.env.BUILD_ID,
+  debug: false,
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+  normalizeDepth: 7,
   // Enable logs to be sent to Sentry
   enableLogs: true,
-
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+  ignoreErrors: [
+    /**
+     * There is a bug in Safari, that causes `AbortError` when fetch is  aborted, and you are in
+     * the middle of reading the response. In Chrome and others it's handled gracefully.
+     *
+     * Ref: https://bugs.webkit.org/show_bug.cgi?id=215771
+     */
+    "AbortError: Fetch is aborted",
+    /**
+     * Cancel errors coming from preventing a navigation change in usePreventNavigation
+     */
+    "CANCEL_ROUTE_CHANGE",
+    /**
+     * Ignore these Next.js errors
+     */
+    "TypeError: Failed to fetch",
+    /**
+     * Ignore userflow infinite errors
+     */
+    "UserflowError: This Userflow.js client has reached a maximum of 100 operations in the last 1 minute",
+  ],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
